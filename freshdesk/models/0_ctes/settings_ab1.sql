ae8ed58e1d656bd3cadfb1149eacf233c17f6271
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2022-11-02 17:52:12 -0500

    feat: clean up ctes references

--- freshdesk/models/0_ctes/settings_ab1.sql
+++ freshdesk/models/0_ctes/settings_ab1.sql
@@ -2,11 +2,10 @@
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
     unique_key = '_airbyte_ab_id',
-    schema = "_airbyte_freshdesk_partner_a",
     tags = [ "top-level-intermediate" ]
 ) }}
 -- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
--- depends_on: {{ source('freshdesk_partner_a', '_airbyte_raw_settings') }}
+-- depends_on: {{ source('cta', '_airbyte_raw_settings') }}
 select
     {{ json_extract_string_array('_airbyte_data', ['portal_languages'], ['portal_languages']) }} as portal_languages,
     {{ json_extract_scalar('_airbyte_data', ['primary_language'], ['primary_language']) }} as primary_language,
@@ -14,7 +13,7 @@ select
     _airbyte_ab_id,
     _airbyte_emitted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at
-from {{ source('freshdesk_partner_a', '_airbyte_raw_settings') }} as table_alias
+from {{ source('cta', '_airbyte_raw_settings') }} as table_alias
 -- settings
 where 1 = 1
 
--- freshdesk/models/0_ctes/tickets_ab1.sql
+++ freshdesk/models/0_ctes/tickets_ab1.sql
@@ -2,11 +2,10 @@
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
     unique_key = '_airbyte_ab_id',
-    schema = "_airbyte_freshdesk_partner_a",
     tags = [ "top-level-intermediate" ]
 ) }}
 -- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
--- depends_on: {{ source('freshdesk_partner_a', '_airbyte_raw_tickets') }}
+-- depends_on: {{ source('cta', '_airbyte_raw_tickets') }}
 select
     {{ json_extract_scalar('_airbyte_data', ['id'], ['id']) }} as id,
     {{ json_extract_scalar('_airbyte_data', ['spam'], ['spam']) }} as spam,
@@ -45,7 +44,7 @@ select
     _airbyte_ab_id,
     _airbyte_emitted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at
-from {{ source('freshdesk_partner_a', '_airbyte_raw_tickets') }} as table_alias
+from {{ source('cta', '_airbyte_raw_tickets') }} as table_alias
 -- tickets
 where 1 = 1
 {{ incremental_clause('_airbyte_emitted_at', this) }}
