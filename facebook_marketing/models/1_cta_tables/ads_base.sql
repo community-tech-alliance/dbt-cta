d474e7f2a230cdfa4d95ef1af8a91987279db55e
Emily Cogsdill <47834372+emilycogsdill@users.noreply.github.com>
2022-11-29 02:54:12 -0700

    feat: add 0_ctes and 1_cta_tables

---
+++ facebook_marketing/models/1_cta_tables/ads_base.sql
@@ -0,0 +1,42 @@
+{% set partitions_to_replace = [
+    'timestamp_trunc(current_timestamp, day)',
+    'timestamp_trunc(timestamp_sub(current_timestamp, interval 1 day), day)'
+] %}
+
+{{ config(
+    cluster_by = "_airbyte_emitted_at",
+    partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = '_airbyte_ads_hashid'
+) }}
+
+-- depends_on: {{ ref('ads_ab3') }}
+select
+     _airbyte_ads_hashid
+    ,_airbyte_emitted_at
+    ,_airbyte_ab_id
+    ,JSON_EXTRACT_SCALAR(creative, "$.id") as creative_id
+    ,id
+    ,name
+    ,status
+    ,adlabels
+    ,adset_id
+    ,bid_info
+    ,bid_type
+    ,creative
+    ,targeting
+    ,account_id
+    ,bid_amount
+    ,campaign_id
+    ,created_time
+    ,source_ad_id
+    ,updated_time
+    ,tracking_specs
+    ,recommendations
+    ,conversion_specs
+    ,effective_status
+    ,last_updated_by_app_id
+from {{ ref('ads_ab3') }}
+
+{% if is_incremental() %}
+where timestamp_trunc(_airbyte_emitted_at, day) in ({{ partitions_to_replace | join(',') }})
+{% endif %}
---
+++ facebook_marketing/models/1_cta_tables/campaigns_base.sql
@@ -0,0 +1,43 @@
+{% set partitions_to_replace = [
+    'timestamp_trunc(current_timestamp, day)',
+    'timestamp_trunc(timestamp_sub(current_timestamp, interval 1 day), day)'
+] %}
+
+{{ config(
+    cluster_by = "_airbyte_emitted_at",
+    partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = '_airbyte_campaigns_hashid'
+) }}
+
+-- depends_on: {{ ref('campaigns_ab3') }}
+select
+     _airbyte_campaigns_hashid
+    ,_airbyte_emitted_at
+    ,_airbyte_ab_id
+    ,id
+    ,name
+    ,adlabels
+    ,objective
+    ,spend_cap
+    ,stop_time
+    ,account_id
+    ,start_time
+    ,buying_type
+    ,issues_info
+    ,bid_strategy
+    ,created_time
+    ,daily_budget
+    ,updated_time
+    ,lifetime_budget
+    ,budget_remaining
+    ,effective_status
+    ,source_campaign_id
+    ,special_ad_category
+    ,smart_promotion_type
+    ,budget_rebalance_flag
+    ,special_ad_category_country
+from {{ ref('campaigns_ab3') }}
+
+{% if is_incremental() %}
+where timestamp_trunc(_airbyte_emitted_at, day) in ({{ partitions_to_replace | join(',') }})
+{% endif %}
