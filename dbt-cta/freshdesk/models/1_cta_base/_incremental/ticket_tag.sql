e0f14a48cd5ea5c85c473a3e44c4bb613621fa8b
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2022-12-09 12:29:44 -0600

    feat: add ticket tables

---
+++ dbt-cta/freshdesk/models/1_cta_base/_incremental/ticket_tag.sql
@@ -0,0 +1,13 @@
+{{ config(
+    unique_key = ["ticket_id", "tag"],
+    materialized = "incremental",
+    incremental_strategy = "merge",
+    on_schema_change = "sync_all_columns",
+) }}
+
+select
+  id as ticket_id,
+  tag
+from {{ ref("tickets_ab3") }} tickets, UNNEST(tickets.tags) as tag
+-- ticket from {{ source('cta', '_airbyte_raw_tickets') }}
+where 1 = 1
