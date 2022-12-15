92299da5d87bb5ffa774db3f50146aa466ea1e47
Emily Cogsdill <emilycogsdill@gmail.com>
2022-12-15 18:25:24 +0100

    switch to prod flavor

--- dbt-cta/google_ads/models/0_ctes/latest_ads.sql
+++ dbt-cta/google_ads/models/0_ctes/latest_ads.sql
@@ -1,9 +1,5 @@
 {%- call statement('latest_partition', fetch_result=True) -%}
-    --PROD:
-    --select max(_PARTITIONDATE) from {{ source('partner','p_AdGroup_1731221521') }};
-
-    --DEV (I couldn't get the _PARTITIONDATE pseudocolumn to work...)
-    select max(date_partition) from {{ source('partner','p_AdGroup_1731221521') }};
+    select max(_PARTITIONDATE) from {{ source('partner','p_Ad_1731221521') }};
 {%- endcall -%}
 
 {%- set partition_time = load_result('latest_partition')['data'][0][0] -%}
@@ -13,7 +9,4 @@ select
   ImageCreativeName,
   CreativeUrlCustomParameters
 from {{ source('partner', 'p_Ad_1731221521') }} as table_alias
---PROD
---where _PARTITIONTIME = '{{ partition_time }}'
---DEV
-where date_partition = '{{ partition_time }}'
+where _PARTITIONTIME = '{{ partition_time }}'
--- dbt-cta/google_ads/models/0_ctes/latest_campaigns.sql
+++ dbt-cta/google_ads/models/0_ctes/latest_campaigns.sql
@@ -1,9 +1,5 @@
 {%- call statement('latest_partition', fetch_result=True) -%}
-    --PROD:
-    --select max(_PARTITIONDATE) from {{ source('partner','p_Campaign_1731221521') }};
-
-    --DEV (I couldn't get the _PARTITIONDATE pseudocolumn to work...)
-    select max(date_partition) from {{ source('partner','p_Campaign_1731221521') }};
+    select max(_PARTITIONDATE) from {{ source('partner','p_Campaign_1731221521') }};
 {%- endcall -%}
 
 {%- set partition_time = load_result('latest_partition')['data'][0][0] -%}
@@ -14,7 +10,4 @@ select
   BudgetId,
   UrlCustomParameters
 from {{ source('partner', 'p_Campaign_1731221521') }} as table_alias
---PROD
---where _PARTITIONTIME = '{{ partition_time }}'
---DEV
-where date_partition = '{{ partition_time }}'
+where _PARTITIONTIME = '{{ partition_time }}'
