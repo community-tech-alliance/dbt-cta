d85a7eb86eebccd3b3df253833835c6469a7bd69
Emily Cogsdill <emilycogsdill@gmail.com>
2022-12-15 11:29:02 +0100

    Update latest_campaigns.sql

--- dbt-cta/google_ads/models/0_ctes/latest_campaigns.sql
+++ dbt-cta/google_ads/models/0_ctes/latest_campaigns.sql
@@ -1,12 +1,4 @@
-{%- call statement('latest_partition', fetch_result=True) -%}
-    --PROD:
-    --select max(_PARTITIONDATE) from {{ source('partner','p_AdGroup_1731221521') }};
-
-    --DEV (I couldn't get the _PARTITIONDATE pseudocolumn to work...)
-    select max(date_partition) from {{ source('partner','p_AdGroup_1731221521') }};
-{%- endcall -%}
-
-{%- set partition_time = load_result('latest_partition')['data'][0][0] -%}
+{% set partition_time = latest_partition(get_custom_database(), get_custom_schema(), 'p_Budget_1731221521') %}
 
 select
   CampaignId,
