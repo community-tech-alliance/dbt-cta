be3ff628caba0a4643491d2023a4ca6a088e03ce
emilyflambe <emilycogsdill@gmail.com>
2022-12-16 21:03:26 +0100

    removing google_ads

--- dbt-cta/google_ads/models/0_ctes/campaigns_with_budget.sql
+++
@@ -1,8 +0,0 @@
-select
-  campaigns.CampaignId,
-  campaigns.CampaignName,
-  campaigns.BudgetId,
-  budgets.TotalAmount,
-  campaigns.UrlCustomParameters
-from {{ ref('latest_campaigns') }} campaigns
-join {{ ref('latest_budgets') }} budgets on campaigns.BudgetId = budgets.BudgetId
