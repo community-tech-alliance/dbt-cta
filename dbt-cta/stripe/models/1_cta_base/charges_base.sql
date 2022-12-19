b9009632b94b45c9635b946e1858a79d29e918e1
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2022-12-19 16:47:16 -0700

    fix: update incremental stripe models merge strategy

--- dbt-cta/stripe/models/1_cta_base/charges_base.sql
+++ dbt-cta/stripe/models/1_cta_base/charges_base.sql
@@ -1,8 +1,7 @@
 {{ config(
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = '_airbyte_ab_id',
-    schema = "stripe_partner_a",
+    unique_key = '_airbyte_charges_hashid',
     tags = [ "top-level" ]
 ) }}
 -- Final base SQL model
--- dbt-cta/stripe/models/1_cta_base/plans_base.sql
+++ dbt-cta/stripe/models/1_cta_base/plans_base.sql
@@ -1,7 +1,7 @@
 {{ config(
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = '_airbyte_ab_id',
+    unique_key = '_airbyte_plans_hashid',
     schema = "stripe_partner_a",
     tags = [ "top-level" ]
 ) }}
