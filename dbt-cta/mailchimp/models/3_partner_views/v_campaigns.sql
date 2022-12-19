47d7dca2e36afcb5508be53fd618b86cf98c39e5
emilyflambe <emilycogsdill@gmail.com>
2022-12-19 10:39:57 +0100

    Update v_campaigns.sql

--- dbt-cta/mailchimp/models/3_partner_views/v_campaigns.sql
+++ dbt-cta/mailchimp/models/3_partner_views/v_campaigns.sql
@@ -1,2 +1,5 @@
 --TODO(): reference this source from a variable configured in the config YAML
+
+-- depends_on: {{ source('partner', 'campaigns') }}
+
 SELECT * FROM {{ source('partner','campaigns') }}
