4370984b180e413fc6a1e24df471bec945cdb2bf
kane <kanelouise@users.noreply.github.com>
2023-02-13 19:00:40 -0800

    fix: make source 'cta' (#76)
    Co-authored-by: kanelouise <kane@techallies.org>

--- dbt-cta/usvote_foundation/models/0_ctes/states_ab1.sql
+++ dbt-cta/usvote_foundation/models/0_ctes/states_ab1.sql
@@ -15,7 +15,7 @@ select
     _airbyte_ab_id,
     _airbyte_emitted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at
-from {{ source('usvote_foundation', '_airbyte_raw_states') }} as table_alias
+from {{ source('cta', '_airbyte_raw_states') }} as table_alias
 -- states
 where 1 = 1
 
