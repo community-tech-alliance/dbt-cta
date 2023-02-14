4ea3573bd48146d801ef2ab115ef0015746b1d59
Jack Welty <jack.welty+github@gmail.com>
2023-02-14 10:18:34 -0500

    feat: add campaigns_base model

---
+++ dbt-cta/mailchimp_jack/models/1_cta_base/campaigns_base.sql
@@ -0,0 +1,45 @@
+{{
+    config(
+        cluster_by="_airbyte_emitted_at",
+        partition_by={
+            "field": "_airbyte_emitted_at",
+            "data_type": "timestamp",
+            "granularity": "day",
+        },
+        unique_key="_airbyte_ab_id",
+    )
+}}
+
+
+select
+    id,
+    type,
+    status,
+    web_id,
+    rss_opts,
+    settings,
+    tracking,
+    send_time,
+    recipients,
+    resendable,
+    archive_url,
+    create_time,
+    emails_sent,
+    social_card,
+    content_type,
+    ab_split_opts,
+    report_summary,
+    delivery_status,
+    long_archive_url,
+    variate_settings,
+    parent_campaign_id,
+    needs_block_refresh,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    {{ current_timestamp() }} as _airbyte_normalized_at,
+    _airbyte_campaigns_hashid
+from {{ ref("campaigns_ab_create_id") }}
+where
+    1 = 1
+    -- {{ incremental_clause('_airbyte_emitted_at') }}
+    
