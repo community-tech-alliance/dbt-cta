3f54aa36ab24b6b477d48a79e676d792d4437e96
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2023-02-24 16:06:34 -0500

    fix: make usvote base models incremental (#89)
    * feat: make base table models incremental so matviews dont break
    
    * feat: make base table models incremental so matviews dont break

--- dbt-cta/usvote_foundation/models/1_cta_base/officials_base.sql
+++ dbt-cta/usvote_foundation/models/1_cta_base/officials_base.sql
@@ -1,4 +1,9 @@
+{% set partitions_to_replace = [
+    'timestamp_trunc(current_timestamp, day)',
+    'timestamp_trunc(timestamp_sub(current_timestamp, interval 1 day), day)'
+] %}
 {{ config(
+    partitions=partitions_to_replace,
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
     unique_key = '_airbyte_ab_id',
@@ -27,5 +32,7 @@ select
     _airbyte_officials_hashid
 from {{ ref('officials_ab3') }}
 -- officials from {{ source('cta', '_airbyte_raw_officials') }}
-where 1 = 1
+{% if is_incremental() %}
+where timestamp_trunc(_airbyte_emitted_at, day) in ({{ partitions_to_replace | join(',') }})
+{% endif %}
 
