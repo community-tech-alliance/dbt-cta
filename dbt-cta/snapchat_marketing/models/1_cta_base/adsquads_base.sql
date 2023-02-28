f3c0723bcd1ce3e4024a01c88cfb067a93ffc155
Jack Welty <jack.welty+github@gmail.com>
2023-02-28 11:06:10 -0500

    feat: swap unique_key of ad_squads

--- dbt-cta/snapchat_marketing/models/1_cta_base/adsquads_base.sql
+++ dbt-cta/snapchat_marketing/models/1_cta_base/adsquads_base.sql
@@ -6,7 +6,7 @@
 {{ config(
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = '_airbyte_adsquads_hashid'
+    unique_key = 'id'
 ) }}
 
 -- depends_on: {{ ref('adsquads_ab3') }}
