3eee2e9e7f1f928aeb8737283b63f71ff420b1cc
Jack Welty <jack.welty+github@gmail.com>
2023-02-28 11:12:10 -0500

    feat: swap ids on remaining "easy" objects

--- dbt-cta/snapchat_marketing/models/1_cta_base/segments_base.sql
+++ dbt-cta/snapchat_marketing/models/1_cta_base/segments_base.sql
@@ -6,7 +6,7 @@
 {{ config(
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = '_airbyte_ab_id'
+    unique_key = 'id'
 ) }}
 
 -- depends_on: {{ ref('segments_ab3') }}
