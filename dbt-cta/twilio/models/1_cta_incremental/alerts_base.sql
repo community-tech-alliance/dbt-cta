7a72b41e68d35cec23d58b4cd4e66c46026b92f6
emilyflambe <emilycogsdill@gmail.com>
2023-03-02 09:51:14 -0700

    science: set `sid` to PK in alerts table (does this do anything?)

--- dbt-cta/twilio/models/1_cta_incremental/alerts_base.sql
+++ dbt-cta/twilio/models/1_cta_incremental/alerts_base.sql
@@ -1,7 +1,7 @@
 {{ config(
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = '_airbyte_ab_id',
+    unique_key = 'sid',
     tags = [ "top-level" ]
 ) }}
 -- Final base SQL model
