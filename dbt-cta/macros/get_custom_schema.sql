ab3f53162a147de748e7bbb1091472d440957a7a
Jack Welty <jack.welty+github@gmail.com>
2023-03-09 15:02:48 -0500

    feat: write elementary artifacts to custom schema

--- dbt-cta/macros/get_custom_schema.sql
+++ dbt-cta/macros/get_custom_schema.sql
@@ -1,4 +1,10 @@
--- see https://docs.getdbt.com/docs/building-a-dbt-project/building-models/using-custom-schemas/#an-alternative-pattern-for-generating-schema-names
+-- see
+-- https://docs.getdbt.com/docs/building-a-dbt-project/building-models/using-custom-schemas/#an-alternative-pattern-for-generating-schema-names
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {{ generate_schema_name_for_env(custom_schema_name, node) }}
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+        {{ generate_schema_name_for_env(custom_schema_name, node) }}
+    {%- else -%}
+        {{ custom_schema_name }}
+    {%- endif -%}
 {%- endmacro %}
