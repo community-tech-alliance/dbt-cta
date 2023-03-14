cdde39c583bde61aab4f116acbd931d9f9d78927
Jack Welty <jack.welty+github@gmail.com>
2023-03-14 17:04:13 -0400

    feat: clean up account_report_base

--- dbt-cta/facebook_marketing/models/1_cta_incremental/account_report_base.sql
+++ dbt-cta/facebook_marketing/models/1_cta_incremental/account_report_base.sql
@@ -1,44 +1,33 @@
-{% set partitions_to_replace = [
-    'timestamp_trunc(current_timestamp, day)',
-    'timestamp_trunc(timestamp_sub(current_timestamp, interval 1 day), day)'
-] %}
-
-{{ config(
-    cluster_by = "_airbyte_emitted_at",
-    partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    partitions = partitions_to_replace,
-    unique_key = '_account_report_hashid'
-) }}
+{{
+    config(
+        cluster_by="_airbyte_emitted_at",
+        partition_by={
+            "field": "_airbyte_emitted_at",
+            "data_type": "timestamp",
+            "granularity": "day",
+        },
+        unique_key="_account_report_hashid",
+    )
+}}
 
 -- depends_on: {{ ref('ads_insights_overall_base') }}
+with
+    aggregations as (
+        select
+            date_start,
+            account_id,
+            account_name,
+            sum(clicks) as clicks,
+            sum(impressions) as impressions,
+            sum(spend) as spend,
+            max(_airbyte_emitted_at) as _airbyte_emitted_at
+        from {{ ref("ads_insights_overall_base") }}
+        group by date_start, account_id, account_name
+    )
 
-with aggregations as (
 select
-    date_start,
-    account_id,
-    account_name,
-    timestamp_trunc(_airbyte_emitted_at, day) as _airbyte_emitted_at,
-    sum(clicks) as clicks,
-    sum(impressions) as impressions,
-    sum(spend) as spend
-from  {{ ref('ads_insights_overall_base') }}
-GROUP BY
-    date_start,
-    account_id,
-    account_name,
-    timestamp_trunc(_airbyte_emitted_at, day)
-)
-
-SELECT
     *,
-    {{ dbt_utils.surrogate_key([
-    'date_start',
-    'account_id',
-    'account_name'
-    ]) }} as _account_report_hashid
-    ,current_timestamp as _airbyte_normalized_at
-FROM aggregations
-
-{% if is_incremental() %}
-where timestamp_trunc(_airbyte_emitted_at, day) in ({{ partitions_to_replace | join(',') }})
-{% endif %}
+    {{ dbt_utils.surrogate_key(["date_start", "account_id", "account_name"]) }}
+    as _account_report_hashid,
+    current_timestamp as _airbyte_normalized_at
+from aggregations
