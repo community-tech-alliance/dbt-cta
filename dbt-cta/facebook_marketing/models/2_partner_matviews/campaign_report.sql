d821536ffc3482312af1c4a080db2ea8bd733627
Jack Welty <jack.welty+github@gmail.com>
2023-03-21 11:10:33 -0400

    feat: refactor matviews

--- dbt-cta/facebook_marketing/models/2_partner_matviews/campaign_report.sql
+++ dbt-cta/facebook_marketing/models/2_partner_matviews/campaign_report.sql
@@ -7,16 +7,5 @@
 -- depends_on: {{ source('cta', 'campaign_report_base') }}
 
 SELECT
-     _campaign_report_hashid
-    ,MAX(date_start) as date_start
-    ,MAX(account_id) as account_id
-    ,MAX(account_name) as account_name
-    ,MAX(campaign_id) as campaign_id
-    ,MAX(campaign_name) as campaign_name
-    ,MAX(clicks) as clicks
-    ,MAX(impressions) as impressions
-    ,MAX(spend) as spend
-    ,MAX(_airbyte_emitted_at) as _airbyte_emitted_at
-    ,MAX(_airbyte_normalized_at) as _airbyte_normalized_at
+    *
 FROM {{ source('cta', 'campaign_report_base') }}
-GROUP BY _campaign_report_hashid
