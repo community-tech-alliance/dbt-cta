69bf83a2ebeef202a3e9d874a5933686b1328c0f
Jack Welty <jack.welty+github@gmail.com>
2023-03-22 16:45:00 -0400

    fix: syntax

--- dbt-cta/edr_report.sh
+++ dbt-cta/edr_report.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 pip install ./elementary
-&& export PATH='$PATH:/home/airflow/.local/bin:/opt/python3.8/bin'
-&& edr send-report \
+export PATH='$PATH:/home/airflow/.local/bin:/opt/python3.8/bin'
+edr send-report \
     --profiles-dir . \
     --project-profile-target cta -c . \
     --slack-token $ELEMENTARY_SLACK_TOKEN \
