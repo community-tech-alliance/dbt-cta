1d1eaf08076f8d2bb9d6c7ba9636ec241539e906
Jack Welty <jack.welty+github@gmail.com>
2023-03-22 16:14:57 -0400

    feat: add edr_report shell script

---
+++ dbt-cta/edr_report.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+pip install ./elementary
+&& export PATH='$PATH:/home/airflow/.local/bin:/opt/python3.8/bin'
+&& edr send-report \
+    --profiles-dir . \
+    --project-profile-target cta -c . \
+    --slack-token $ELEMENTARY_SLACK_TOKEN \
+    --slack-channel-name "elementary-alerts" \
+    --project-name $SYNC_NAME \
+    --env dev
