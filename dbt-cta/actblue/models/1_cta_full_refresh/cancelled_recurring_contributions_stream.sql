b93a57b8a39ad220b7cc08fc97f4403f7b036c22
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2023-03-29 17:00:10 -0500

    feat: add models for new ActBlue Stream

---
+++ dbt-cta/actblue/models/1_cta_full_refresh/cancelled_recurring_contributions_stream.sql
@@ -0,0 +1,42 @@
+{{ config(
+    cluster_by = "_airbyte_emitted_at",
+    partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = 'Receipt_ID',
+    materialized = "incremental",
+    incremental_strategy = "merge",
+    on_schema_change = "sync_all_columns",
+    tags = [ "top-level" ]
+) }}
+-- Final base SQL model
+-- depends_on: {{ ref('cancelled_recurring_contributions_stream_ab3') }}
+select
+    Donor_ZIP,
+    Donor_City,
+    Receipt_ID,
+    Donor_Email,
+    Donor_Phone,
+    Donor_State,
+    Cancelled_On,
+    Total_Amount,
+    Donor_Address,
+    Donor_Country,
+    Express_Donor,
+    Donor_Employer,
+    Reference_Code,
+    Donor_Last_Name,
+    Donor_First_Name,
+    Donor_Occupation,
+    Recurrence_Amount,
+    Bump_Recurring_Seen,
+    Recurrence_Frequency,
+    Initial_Pledge_Length,
+    Bump_Recurring_Succeeded,
+    Initial_Contribution_Date,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    {{ current_timestamp() }} as _airbyte_normalized_at,
+    _airbyte_cancelled_recurring_contributions_stream_hashid
+from {{ ref('cancelled_recurring_contributions_stream_ab2') }}
+-- cancelled_recurring_contributions_stream from {{ source('cta', '_airbyte_raw_cancelled_recurring_contributions_stream') }}
+where 1 = 1
+
