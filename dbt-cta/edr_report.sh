147313a9598149a201c7cb558c7cf5084dd7aca3
Jack Welty <jack.welty+github@gmail.com>
2023-04-03 10:19:34 -0400

    dep: remove shell script

--- dbt-cta/edr_report.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-export PATH='$PATH:/home/airflow/.local/bin:/opt/python3.8/bin'
-edr send-report \
-    --profiles-dir . \
-    --project-profile-target cta -c . \
-    --slack-token $ELEMENTARY_SLACK_TOKEN \
-    --slack-channel-name "elementary-alerts" \
-    --project-name $SYNC_NAME \
-    --env dev
