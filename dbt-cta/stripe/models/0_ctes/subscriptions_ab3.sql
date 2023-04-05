6c04ae37b8c6c6fa5439f8664befe716a060c9e9
emilyflambe <emilycogsdill@gmail.com>
2023-04-05 12:50:00 -0400

    hotfix: removing more "partner_a" schemas in config

--- dbt-cta/stripe/models/0_ctes/subscriptions_ab3.sql
+++ dbt-cta/stripe/models/0_ctes/subscriptions_ab3.sql
@@ -2,7 +2,6 @@
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
     unique_key = '_airbyte_ab_id',
-    schema = "_airbyte_stripe_partner_a",
     tags = [ "top-level-intermediate" ]
 ) }}
 -- SQL model to build a hash column based on the values of this record
