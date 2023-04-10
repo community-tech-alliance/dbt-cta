080feb3607939cfb59d771ed2973a9e0b8671488
Flambé <47834372+emily-flambe@users.noreply.github.com>
2023-04-10 18:06:49 -0400

    hotfix: remove hardcoded schema from stackadapt ctes (#120)

--- dbt-cta/stackadapt/models/0_ctes/account_native_ads_stats_creatives_ab3.sql
+++ dbt-cta/stackadapt/models/0_ctes/account_native_ads_stats_creatives_ab3.sql
@@ -1,7 +1,6 @@
 {{ config(
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    schema = "_airbyte_vfp_stackadapt_raw_v2",
     tags = [ "nested-intermediate" ]
 ) }}
 -- SQL model to build a hash column based on the values of this record
