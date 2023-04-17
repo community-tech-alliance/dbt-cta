e6b7e2248cb2d73de5a44474b589e1b588f4a10c
Jack Welty <jack.welty+github@gmail.com>
2023-04-17 16:31:15 -0400

    style: sqlfmt

--- dbt-cta/monitoring/models/reporting/pipeline_health/rpt_pipeline_health.sql
+++ dbt-cta/monitoring/models/reporting/pipeline_health/rpt_pipeline_health.sql
@@ -21,34 +21,43 @@ with
     add_most_recent_run as (
         select
             *,
-            case when rank() over (
-                partition by sync_name, partner_name, extract(date from run_started_at)
-                order by run_started_at desc
-            ) = 1 then 1 else 0 end as most_recent_run_per_day,
+            case
+                when
+                    rank() over (
+                        partition by
+                            sync_name, partner_name, extract(date from run_started_at)
+                        order by run_started_at desc
+                    )
+                    = 1
+                then 1
+                else 0
+            end as most_recent_run_per_day,
         from compute_successes
     ),
     add_run_time as (
         select *, datetime_diff(run_completed_at, run_started_at, minute) as runtime
         from add_most_recent_run
     ),
-    final as (select 
-    sync_name
-    , partner_name
-    , airflow_run_id
-    , num_steps
-    , run_started_at
-    , run_completed_at
-    , test_errors
-    , successful_tests
-    , total_tests
-    , model_errors
-    , successful_models
-    , total_models
-    , success
-    , success_int
-    , most_recent_run_per_day
-    , runtime
-     from add_run_time)
+    final as (
+        select
+            sync_name,
+            partner_name,
+            airflow_run_id,
+            num_steps,
+            run_started_at,
+            run_completed_at,
+            test_errors,
+            successful_tests,
+            total_tests,
+            model_errors,
+            successful_models,
+            total_models,
+            success,
+            success_int,
+            most_recent_run_per_day,
+            runtime
+        from add_run_time
+    )
 
 select *
 from final
