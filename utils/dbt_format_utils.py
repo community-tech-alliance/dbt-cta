0a627dda29352c5e5fd5e90159ef20c3e76c218f
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2023-04-21 15:29:40 -0500

    style: run black

--- utils/dbt_format_utils.py
+++ utils/dbt_format_utils.py
@@ -7,6 +7,7 @@ from shutil import move
 
 import requests
 
+
 # Entrypoint, parses input args and call corresponding functions
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -16,7 +17,9 @@ def parse_args():
 
 def main():
     parser = argparse.ArgumentParser()
-    sub_parsers = parser.add_subparsers(title="action", description="Choose which action to run", dest="action")
+    sub_parsers = parser.add_subparsers(
+        title="action", description="Choose which action to run", dest="action"
+    )
 
     # Rename Base Tables input parser
     rename_parser = sub_parsers.add_parser("renameBaseTables")
@@ -68,7 +71,12 @@ def main():
         if not args.outputPath:
             raise Exception("Make sure you specify the --outputPath argument")
         # Run selected action
-        download_cacher_script(args.cacherSnippetGUID, args.cacherApiKey, args.cacherApiToken, args.outputPath)
+        download_cacher_script(
+            args.cacherSnippetGUID,
+            args.cacherApiKey,
+            args.cacherApiToken,
+            args.outputPath,
+        )
 
     elif args.action == "restructAirbyteDbt":
         # Check Args for Action
@@ -79,47 +87,48 @@ def main():
         # Run selected action
         restructure_airbyte_dbt(args.airbyteWorkspacePath, args.dbtCtaPath)
     else:
-        raise Exception("Please enter a supported action -> renameBaseTables, createMatViews, getCacherScript, restructAirbyteDbt")
+        raise Exception(
+            "Please enter a supported action -> renameBaseTables, createMatViews, getCacherScript, restructAirbyteDbt"
+        )
 
-    
 
 # Function to grab Airbyte Generated dbt and move into CTA structured folder (✨Progify it✨)
 def restructure_airbyte_dbt(airbyte_workspace_path, dbt_cta_path):
-
     # Mapping of Airbyte folders -> CTA folders
     airbyte_cta_dir_mapping = {
         "airbyte_ctes": "0_ctes",
         "airbyte_tables": "1_cta_full_refresh",
-        "airbyte_incremental": "1_cta_incremental"
+        "airbyte_incremental": "1_cta_incremental",
     }
 
-    # Go through and move all files from Airbyte export into 'dbt-cta' directories     
+    # Go through and move all files from Airbyte export into 'dbt-cta' directories
     for airbyte_dir, cta_dir in airbyte_cta_dir_mapping.items():
-
         # Get all sql files that should be moved for current mapping
-        files_to_move = glob(f"{airbyte_workspace_path}/**/{airbyte_dir}/**/*.sql", recursive=True)
+        files_to_move = glob(
+            f"{airbyte_workspace_path}/**/{airbyte_dir}/**/*.sql", recursive=True
+        )
 
         if files_to_move:  # Only run if there were any files found
             # Make sure target path is created
-            dbt_cta_path = dbt_cta_path if dbt_cta_path[-1] != "/" else dbt_cta_path[:-1]
+            dbt_cta_path = (
+                dbt_cta_path if dbt_cta_path[-1] != "/" else dbt_cta_path[:-1]
+            )
             target_path = f"{dbt_cta_path}/models/{cta_dir}"
             os.makedirs(target_path, exist_ok=True)
 
             # Move files to target path
             for file_path in files_to_move:
                 file_name = file_path.split("/")[-1]
-                moved_file = move(
-                    src=file_path,
-                    dst=f"{target_path}/{file_name}"
-                )
+                moved_file = move(src=file_path, dst=f"{target_path}/{file_name}")
                 print(f"Moved {file_path} -> {moved_file}")
-    
+
     # Now lets move over the sources.yml file
-    sources_file = glob(f"{airbyte_workspace_path}/**/models/**/sources.yml", recursive=True)
+    sources_file = glob(
+        f"{airbyte_workspace_path}/**/models/**/sources.yml", recursive=True
+    )
     if sources_file:
         move_source_file = move(
-            src=sources_file[0],
-            dst=f"{dbt_cta_path}/models/sources.yml"
+            src=sources_file[0], dst=f"{dbt_cta_path}/models/sources.yml"
         )
         print(f"Moved {sources_file[0]} -> {dbt_cta_path}/models/sources.yml")
     else:
@@ -130,17 +139,17 @@ def restructure_airbyte_dbt(airbyte_workspace_path, dbt_cta_path):
     modify_full_refresh_models(f"{dbt_cta_path}/models/1_cta_full_refresh")
     modify_dbt_models(f"{dbt_cta_path}/models/1_cta_incremental", "incremental")
 
-# Function to modify ctes and incremental models. For these its just gonna be 
+
+# Function to modify ctes and incremental models. For these its just gonna be
 # swapping out the config section, and changing source to cta.
 def modify_dbt_models(path_to_models, model_type):
-
     # Default Config Section thats gonna be used to replace Airbyte generated config
     config = (
-        '{{ config(\n'
+        "{{ config(\n"
         '    cluster_by = "_airbyte_emitted_at",\n'
         '    partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},\n'
         '    unique_key = "_airbyte_ab_id"\n'
-        ') }}\n'
+        ") }}\n"
     )
     stop_skip_pattern = "-- SQL model" if model_type == "cte" else "-- Final base SQL"
 
@@ -162,7 +171,7 @@ def modify_dbt_models(path_to_models, model_type):
                     if skip_line:
                         continue
                     # Substitute source with 'cta' if regex match
-                    line = re.sub('(?<=source\(\').*?(?=\', \'_airbyte_raw_)', "cta", line)
+                    line = re.sub("(?<=source\(').*?(?=', '_airbyte_raw_)", "cta", line)
                     temp_file.write(line)
             temp_file.seek(0)
             with open(f"{model_file_path}", "w") as output_file:
@@ -175,22 +184,22 @@ def modify_full_refresh_models(path_to_models):
     # Default Config Section thats gonna be used to replace Airbyte generated config.
     # This also contains the partition replacement header.
     partition_replacement_header = (
-        '{% set partitions_to_replace = [\n'
+        "{% set partitions_to_replace = [\n"
         '    "timestamp_trunc(current_timestamp, day)",\n'
         '    "timestamp_trunc(timestamp_sub(current_timestamp, interval 1 day), day)"\n'
-        '] %}\n'
-        '{{ config(\n'
+        "] %}\n"
+        "{{ config(\n"
         '    cluster_by = "_airbyte_emitted_at",\n'
         '    partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},\n'
-        '    partitions = partitions_to_replace,\n'
+        "    partitions = partitions_to_replace,\n"
         '    unique_key = "_airbyte_ab_id"\n'
-        ') }}\n'
+        ") }}\n"
     )
 
     partition_replacement_footer = (
-        '{% if is_incremental() %}\n'
+        "{% if is_incremental() %}\n"
         'where timestamp_trunc(_airbyte_emitted_at, day) in ({{ partitions_to_replace | join(",") }})\n'
-        '{% endif %}'
+        "{% endif %}"
     )
 
     # Get all full-refresh Models
@@ -214,7 +223,7 @@ def modify_full_refresh_models(path_to_models):
                     if skip_line:
                         continue
                     # Substitute source with 'cta' if regex match
-                    line = re.sub('(?<=source\(\').*?(?=\', \'_airbyte_raw_)', "cta", line)
+                    line = re.sub("(?<=source\(').*?(?=', '_airbyte_raw_)", "cta", line)
                     temp_file.write(line)
             temp_file.write(partition_replacement_footer)
             temp_file.seek(0)
@@ -229,7 +238,7 @@ def download_cacher_script(script_guid, api_key, api_token, output_path):
     headers = {
         "cache-control": "no-cache",
         "x-api-key": api_key,
-        "x-api-token": api_token
+        "x-api-token": api_token,
     }
     resp = requests.get(cacher_url, headers=headers)
     resp.raise_for_status()
@@ -240,18 +249,25 @@ def download_cacher_script(script_guid, api_key, api_token, output_path):
     cta_snippets = cta_team["library"]["snippets"]
 
     # Find target snippet
-    target_script = next(snippet for snippet in cta_snippets if snippet.get("guid") == script_guid)
+    target_script = next(
+        snippet for snippet in cta_snippets if snippet.get("guid") == script_guid
+    )
     script_content = target_script.get("files")[0]["content"]
 
     with open(output_path, "w") as output_file:
         output_file.write(script_content)
 
-    print(f"Cacher Snippet {target_script.get('title', script_guid)} downloaded to {output_path}")
-    
+    print(
+        f"Cacher Snippet {target_script.get('title', script_guid)} downloaded to {output_path}"
+    )
+
+
 # Function to append "_base" to all files in a directory
 def add_base_to_filenames(base_tables_path):
     base_tables = [
-        f for f in os.listdir(base_tables_path) if os.path.isfile(os.path.join(base_tables_path, f))
+        f
+        for f in os.listdir(base_tables_path)
+        if os.path.isfile(os.path.join(base_tables_path, f))
     ]
     base_tables.sort()
     for table_file in base_tables:
@@ -273,9 +289,10 @@ def add_base_to_filenames(base_tables_path):
 
 
 def create_matview_dbt_files_from_base(base_tables_path, output_path):
-
     # Make sure path structure is consistent
-    base_tables_path = base_tables_path if base_tables_path[-1] != "/" else base_tables_path[:-1]
+    base_tables_path = (
+        base_tables_path if base_tables_path[-1] != "/" else base_tables_path[:-1]
+    )
     output_path = output_path if output_path[-1] != "/" else output_path[:-1]
     # Check if output exists
     isExist = os.path.exists(output_path)
@@ -285,7 +302,9 @@ def create_matview_dbt_files_from_base(base_tables_path, output_path):
         print(f"{output_path} is created!")
 
     base_tables = [
-        f for f in os.listdir(base_tables_path) if os.path.isfile(os.path.join(base_tables_path, f))
+        f
+        for f in os.listdir(base_tables_path)
+        if os.path.isfile(os.path.join(base_tables_path, f))
     ]
     for table in base_tables:
         with open(f"{base_tables_path}/{table}", "r") as infile:
