1fc573d912e2c868cf02878e57b2957a421a0cbb
emilyflambe <emilycogsdill@gmail.com>
2023-04-24 15:14:16 -0600

    fix: select * for now

--- dbt-cta/empower/models/2_partner_matviews/ctaResults_answerIdsByPromptId.sql
+++ dbt-cta/empower/models/2_partner_matviews/ctaResults_answerIdsByPromptId.sql
@@ -1,10 +1,3 @@
 select
-    _13805,
-    _13198,
-    _13683,
-    _13197,
-    _13684,
-    _13800,
-    _13303,
-    _13304,
+    *
 from {{ source('cta','ctaresults_answeridsbypromptid_base') }}
--- dbt-cta/empower/models/2_partner_matviews/ctaResults_answers.sql
+++ dbt-cta/empower/models/2_partner_matviews/ctaResults_answers.sql
@@ -1,5 +1,3 @@
 select
-    _11,
-    _1,
-    _10,
+    *
 from {{ source('cta','ctaresults_answers_base') }}
