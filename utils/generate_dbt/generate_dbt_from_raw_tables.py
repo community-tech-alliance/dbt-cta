e76f6bb5531f9773c966b540d29660b723d71734
emilyflambe <emilycogsdill@gmail.com>
2023-04-29 08:30:48 -0600

    rename: generate_dbt_from_raw_tables.py

--- utils/generate_dbt/generate_dbt_from_raw_tables.py
+++ utils/generate_dbt/generate_dbt_from_raw_tables.py
@@ -1,12 +1,15 @@
 """
-generate_dbt.py
+generate_dbt_from_raw_tables.py
 
 Takes a BigQuery dataset containing raw tables and generates "cta base" models from them.
 This script assumes the following:
 
-- the raw tables are already normalized
-(TODO: add a mode that parses an unflattened JSON, such as _airbyte_data)
+- the raw tables are already normalized and exist in a BQ dataset
+(TODO: write a script that parses an unflattened JSON, such as _airbyte_data)
+(TODO: chain that together with this one using a shell script and gum (a la cta_dbt_helper.sh)
+(TODO: maybe add these scripts as functionality in cta_dbt_helper?)
 - all tables in the BQ dataset should have a model created
+- base tables should be created in the same dataset as the raw tables
 - all tables update using full refresh overwrite
 (that is, whatever process is delivering those tables is dropping and recreating them on each run)
 (TODO: add a way to create incremental models - tbd how that would work...)
@@ -25,7 +28,7 @@ Returns:
 
 Example Usage:
 
-    pipenv run python generate_dbt.py -p PROJECT_ID -d flambe
+    pipenv run python generate_dbt_from_raw_tables.py -p PROJECT_ID -d flambe
 """
 
 import argparse
@@ -41,21 +44,45 @@ def main():
     parser = argparse.ArgumentParser(description='Generate dbt files.')
     parser.add_argument('--project_id',
                         '-p',
-                        help='Google project id.')
+                        help='Google project id.'
+                        )
     parser.add_argument('--dataset_id',
                         '-d',
-                        help='Dataset id.')
-    # Add an optional argument for the output file
-    parser.add_argument('--output',
+                        help='Dataset id.'
+                        )
+    # Optional arguments
+    parser.add_argument('--unique_key',
+                        '-uk',
+                        help='Unique key for tables in this dataset'
+                             ' (default: _cta_sync_rowid',
+                        default='_cta_sync_rowid'
+                        )
+    parser.add_argument('--datetime_field',
+                        '-dt',
+                        help='Datetime field for partitioning'
+                             ' (default: _cta_sync_datetime_utc',
+                        default='_cta_sync_datetime_utc'
+                        )
+    parser.add_argument('--output_path',
                         '-o',
-                        help='Path to the output ZIP.',
-                        default='dbt_models.zip')
+                        help='Path to the output ZIP (default: dbt_models.zip',
+                        default='dbt_models.zip'
+                        )
     args = parser.parse_args()
 
-    # Set up the BigQuery client and list all tables in the dataset
-    client = bigquery.Client(project=args.project_id)
+    # Parse the arguments
+    project_id = args.project_id
     dataset_id = args.dataset_id
+    output_path = args.output_path
+    unique_key = args.unique_key
+    datetime_field = args.datetime_field
+
+    # Set up the BigQuery client and list all tables in the dataset
+    client = bigquery.Client(project=project_id)
     tables = list(client.list_tables(dataset_id))
+
+    # Include only tables with "raw" in the name
+    tables = [table for table in tables if 'raw' in table.table_id]
 
     with tempfile.TemporaryDirectory() as temp_dir_TODO:
         # Create the sources.yml file
@@ -72,19 +99,29 @@ def main():
         }
 
         temp_dir = "temp_dir"
+        sync_dir = "flambe" #dataset_id
         models_dir = "models"
         models_subdir = "1_cta_full_refresh"
 
         # Join the path segments
-        models_subdir_path = os.path.join(temp_dir, models_dir, models_subdir)
+        models_subdir_path = os.path.join(temp_dir,
+                                          sync_dir,
+                                          models_dir,
+                                          models_subdir
+                                          )
 
         # Create the new directories
         os.makedirs(models_subdir_path, exist_ok=True)
 
         # Write the sources.yml file
-        sources_yml_path = os.path.join(temp_dir, models_dir, "sources.yml")
+        sources_yml_path = os.path.join(temp_dir, sync_dir, models_dir, "sources.yml")
         with open(sources_yml_path, "w") as sources_yml_file:
-            yaml.dump(sources_dict, sources_yml_file, sort_keys=False, default_flow_style=False, default_style='')
+            yaml.dump(sources_dict,
+                      sources_yml_file,
+                      sort_keys=False,
+                      default_flow_style=False,
+                      default_style=''
+                      )
 
         # Iterate over the tables and generate a SQL file for each
         for table in tables:
@@ -94,10 +131,12 @@ def main():
 
             # Define the SQL query
             table_schema_fields = ",\n    ".join(
-                f"CAST({field.name} AS {field.field_type}) AS {field.name}" for field in table_schema
+                f"CAST(`{field.name}` AS {field.field_type}) AS `{field.name}`"
+                for field in table_schema
             )
 
-            concat_fields = ",\n                                        ".join(field.name for field in table_schema)
+            concat_fields = ",\n                                        "\
+                .join(f"`{field.name}`" for field in table_schema)
 
             sql = f"""SELECT
     {table_schema_fields},
@@ -105,25 +144,26 @@ def main():
 FROM {{{{ source('cta', '{table.table_id}') }}}}
 
 {{% if is_incremental() %}}
-where timestamp_trunc(_airbyte_emitted_at, day) in ({{{{ partitions_to_replace | join(",") }}}})
+where timestamp_trunc({datetime_field}, day) in ({{{{ partitions_to_replace | join(",") }}}})
 {{% endif %}}
             """
 
             # Write the SQL query to a file
-            model_name = table.table_id.replace('raw', 'base') if 'raw' in table.table_id else table.table_id+"_base"
-            file_path = os.path.join(temp_dir, models_dir, models_subdir, f"{model_name}.sql")
+            model_name = table.table_id.replace('raw', 'base')\
+                if 'raw' in table.table_id else table.table_id+"_base"
+            file_path = os.path.join(models_subdir_path, f"{model_name}.sql")
             with open(file_path, "w") as f:
-                dbt_config = """{% set partitions_to_replace = [
+                dbt_config = f"""{{% set partitions_to_replace = [
     "timestamp_trunc(current_timestamp, day)",
     "timestamp_trunc(timestamp_sub(current_timestamp, interval 1 day), day)"
-] %}
+] %}}
 
-{{config(
+{{{{config(
     cluster_by="_cta_sync_datetime_utc",
-    partition_by={"field": "_cta_sync_datetime_utc", "data_type": "timestamp", "granularity": "day"},
+    partition_by={{"field": "{datetime_field}", "data_type": "timestamp", "granularity": "day"}},
     partitions=partitions_to_replace,
-    unique_key="_unique_row_id"
-)}}
+    unique_key="{unique_key}"
+)}}}}
 
 -- Final base SQL model
 """
@@ -132,7 +172,7 @@ where timestamp_trunc(_airbyte_emitted_at, day) in ({{{{ partitions_to_replace |
 
     # Write the contents of temp_dir to a ZIP file
 
-    with zipfile.ZipFile(args.output, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(temp_dir):
             for file in files:
                 # Get the full path of the file
@@ -140,7 +180,7 @@ where timestamp_trunc(_airbyte_emitted_at, day) in ({{{{ partitions_to_replace |
                 # Add the file to the ZIP file using a relative path
                 zipf.write(file_path, os.path.relpath(file_path, temp_dir))
 
-    print(f"ZIP file '{args.output}' created.")
+    print(f"ZIP file '{output_path}' created.")
 
 
 if __name__ == '__main__':
