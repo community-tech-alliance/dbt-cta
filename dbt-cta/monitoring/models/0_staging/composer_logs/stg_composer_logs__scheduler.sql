5999585f91b1236816f6f4a3ad0fefa29c59649a
Jack Welty <jack.welty+github@gmail.com>
2023-05-02 15:13:14 -0400

    style: sqlfmt

--- dbt-cta/monitoring/models/0_staging/composer_logs/stg_composer_logs__scheduler.sql
+++ dbt-cta/monitoring/models/0_staging/composer_logs/stg_composer_logs__scheduler.sql
@@ -1,22 +1,22 @@
-with source as (select * from {{ source("composer_logs", "airflow_scheduler") }})
+with
+    source as (select * from {{ source("composer_logs", "airflow_scheduler") }}),
+    unpack_nested_fields as (
+        select
+            *,
+            resource.type as resource_type,
+            resource.labels.project_id as project_id,
+            resource.labels.environment_name as environment_name
 
-, unpack_nested_fields as (
+        from source
+    ),
+    rename as (
+        select
+            * except (insertid, timestamp, receivetimestamp),
+            insertid as insert_id,
+            timestamp as log_timestamp,
+            receivetimestamp as received_timestamp
+        from unpack_nested_fields
+    )
+
 select *
-, resource.type as resource_type
-, resource.labels.project_id as project_id
-, resource.labels.environment_name as environment_name
-
-from source
-)
-
-, rename as (
-    select * except(insertId, timestamp, receiveTimestamp)
-    , insertId as insert_id
-    , timestamp as log_timestamp
-    , receiveTimestamp as received_timestamp
-    from unpack_nested_fields
-)
-
-select * from rename
-
-
+from rename
--- dbt-cta/monitoring/models/1_intermediate/composer_logs/int_parse_scheduler_logs.sql
+++ dbt-cta/monitoring/models/1_intermediate/composer_logs/int_parse_scheduler_logs.sql
@@ -5,7 +5,7 @@ with
         select *, split(textpayload, 'DagRun Finished: ')[offset(1)] as log_data
         from source
     ),
-    
+
     /* The textpayload column contains a comma-separated list of key-value pairs, i.e.:
         dag_id=example_dag, execution_date=2020-01-01T00:00:00+00:00, run_id=manual__2020-01-01T00:00:00+00:00, ...
     */
@@ -40,43 +40,45 @@ with
 
     unpack_values as (
         select
-            insert_id
-            , log_timestamp
-            , received_timestamp
-            , project_id
-            , environment_name
+            insert_id,
+            log_timestamp,
+            received_timestamp,
+            project_id,
+            environment_name
 
             {% for field in fields %}
             ,
             (
-                select value from unnest(generate_struct.log_values) where key = '{{ field }}'
+                select value
+                from unnest(generate_struct.log_values)
+                where key = '{{ field }}'
             ) as {{ field }}
             {% endfor %}
         from generate_struct
 
+    ),
+    cast_data_types as (
+        select
+            insert_id,
+            cast(log_timestamp as timestamp) as log_timestamp,
+            cast(received_timestamp as timestamp) as received_timestamp,
+            project_id,
+            environment_name,
+            dag_id,
+            cast(execution_date as timestamp) as execution_date,
+            run_id,
+            cast(run_start_date as timestamp) as run_start_date,
+            cast(run_end_date as timestamp) as run_end_date,
+            cast(run_duration as float64) as run_duration,
+            state,
+            external_trigger,
+            run_type,
+            cast(data_interval_start as timestamp) as data_interval_start,
+            cast(data_interval_end as timestamp) as data_interval_end,
+            dag_hash
+
+        from unpack_values
     )
 
-, cast_data_types as (
-select 
-            insert_id
-            , cast(log_timestamp as timestamp) as log_timestamp
-            , cast(received_timestamp as timestamp) as received_timestamp
-            , project_id
-            , environment_name
-            , dag_id
-            , cast(execution_date as timestamp) as execution_date
-            , run_id
-            , cast(run_start_date as timestamp) as run_start_date
-            , cast(run_end_date as timestamp) as run_end_date
-            , cast(run_duration as float64) as run_duration
-            , state
-            , external_trigger
-            , run_type
-            , cast(data_interval_start as timestamp) as data_interval_start
-            , cast(data_interval_end as timestamp) as data_interval_end
-            , dag_hash
-
-from unpack_values
-)
-
-select * from cast_data_types
+select *
+from cast_data_types
