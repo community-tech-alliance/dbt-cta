34b838f32724af99a1274fcbd4fd66206d71f381
Jack Welty <jack.welty+github@gmail.com>
2023-05-03 16:49:34 -0400

    feat: add num_task_runs

--- dbt-cta/monitoring/models/1_intermediate/pipeline_health/int_group_by_run.sql
+++ dbt-cta/monitoring/models/1_intermediate/pipeline_health/int_group_by_run.sql
@@ -1,20 +1,19 @@
 with
-    source as (select * from {{ ref("int_group_by_invocation") }}
-    -- We only want the most recent "invocation_type" per invocation
-    where most_recent_invocation_type = 1),
+    source as (select * from {{ ref("int_group_by_invocation") }}),
     group_by_airflow_run as (
 
         select
             sync_name,
             partner_name,
             airflow_run_id,
-            count(distinct invocation_id) as num_steps,
+            count(distinct invocation_type) as num_steps,
+            count(distinct invocation_id) as num_steps_run,
             min(run_started_at) as run_started_at,
             max(run_completed_at) as run_completed_at,
-            coalesce(sum(test_errors),0) as test_errors,
-            coalesce(sum(total_tests),0) as total_tests,
-            coalesce(sum(model_errors),0) as model_errors,
-            coalesce(sum(total_models),0) as total_models,
+            coalesce(sum(case when most_recent_invocation_type = 1 then test_errors else null end),0) as test_errors,
+            coalesce(sum(case when most_recent_invocation_type = 1 then total_tests else null end),0) as total_tests,
+            coalesce(sum(case when most_recent_invocation_type = 1 then model_errors else null end),0) as model_errors,
+            coalesce(sum(case when most_recent_invocation_type = 1 then total_models else null end),0) as total_models,
         from source
         group by 1, 2, 3
     ),
