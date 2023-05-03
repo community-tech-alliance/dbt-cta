96703c7136fc6c2b35b968880ad62ef271a40c87
Jack Welty <jack.welty+github@gmail.com>
2023-05-03 14:20:29 -0400

    feat: refactor group_by_run

--- dbt-cta/monitoring/models/1_intermediate/pipeline_health/int_group_by_run.sql
+++ dbt-cta/monitoring/models/1_intermediate/pipeline_health/int_group_by_run.sql
@@ -1,5 +1,7 @@
 with
-    source as (select * from {{ ref("int_group_by_invocation_status_type") }}),
+    source as (select * from {{ ref("int_group_by_invocation") }}
+    -- We only want the most recent "invocation_type" per invocation
+    where most_recent_invocation_type = 1),
     group_by_airflow_run as (
 
         select
@@ -9,30 +11,10 @@ with
             count(distinct invocation_id) as num_steps,
             min(run_started_at) as run_started_at,
             max(run_completed_at) as run_completed_at,
-            coalesce(
-                sum(
-                    case
-                        when resource_type = 'test' and status not in ('pass', 'warn')
-                        then count
-                    end
-                ),
-                0
-            ) as test_errors,
-            coalesce(
-                sum(
-                    case
-                        when resource_type = 'model' and status not in ('success')
-                        then count
-                    end
-                ),
-                0
-            ) as model_errors,
-            coalesce(
-                sum(case when resource_type = 'test' then count end), 0
-            ) as total_tests,
-            coalesce(
-                sum(case when resource_type = 'model' then count end), 0
-            ) as total_models
+            coalesce(sum(test_errors),0) as test_errors,
+            coalesce(sum(total_tests),0) as total_tests,
+            coalesce(sum(model_errors),0) as model_errors,
+            coalesce(sum(total_models),0) as total_models,
         from source
         group by 1, 2, 3
     ),
