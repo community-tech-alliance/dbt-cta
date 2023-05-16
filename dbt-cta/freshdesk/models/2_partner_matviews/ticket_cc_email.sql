da04c50e3a9056e135c420d73c5f273327decea8
emilyflambe <emilycogsdill@gmail.com>
2023-05-16 09:10:16 -0400

    hotfix: remove stray `_airbyte_emitted_at` fields

--- dbt-cta/freshdesk/models/2_partner_matviews/ticket_cc_email.sql
+++ dbt-cta/freshdesk/models/2_partner_matviews/ticket_cc_email.sql
@@ -1,7 +1,6 @@
 -- Final Materialized View SQL model
 -- depends_on: {{ ref('ticket_base') }}
 select
-  _airbyte_emitted_at,
   id as ticket_id,
   email
 from {{ source('cta', 'ticket_base') }} tickets, UNNEST(tickets.cc_emails) as email
--- dbt-cta/freshdesk/models/2_partner_matviews/ticket_tag.sql
+++ dbt-cta/freshdesk/models/2_partner_matviews/ticket_tag.sql
@@ -1,7 +1,6 @@
 -- Final Materialized View SQL model
 -- depends_on: {{ ref('ticket_base') }}
 select
-  _airbyte_emitted_at,
   id as ticket_id,
   tag
 from {{ source('cta', 'ticket_base') }} tickets, UNNEST(tickets.tags) as tag
