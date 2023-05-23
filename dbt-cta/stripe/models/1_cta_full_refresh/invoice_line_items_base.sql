61cb011c3d2df8a96689a5afdb7cb12a22e35a7d
Flambé <47834372+emily-flambe@users.noreply.github.com>
2023-05-23 17:16:28 -0400

    feat (stripe): add `partitions=partitions_to_replace` to 1_cta_full_refresh configs (#160)

--- dbt-cta/stripe/models/1_cta_full_refresh/invoice_line_items_base.sql
+++ dbt-cta/stripe/models/1_cta_full_refresh/invoice_line_items_base.sql
@@ -6,8 +6,8 @@
 {{ config(
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
+    partitions=partitions_to_replace,
     unique_key = '_airbyte_ab_id',
-    
     tags = [ "top-level" ]
 ) }}
 -- Final base SQL model
