9b08407e9c0c3a03600e111c9f15e4dab67189d4
emily-flambe <emilycogsdill@gmail.com>
2023-05-24 19:58:22 -0400

    fix: typos in `{{ ref('...`

--- dbt-cta/action_network/models/2_partner_matviews/email_campaign_members.sql
+++ dbt-cta/action_network/models/2_partner_matviews/email_campaign_members.sql
@@ -5,4 +5,4 @@ select
     updated_at,
     email_campaign_id,
     _airbyte_email_campaign_members_hashid
-from {{ ref('cta','email_campaign_members_base') }}
+from {{ ref('email_campaign_members_base') }}
