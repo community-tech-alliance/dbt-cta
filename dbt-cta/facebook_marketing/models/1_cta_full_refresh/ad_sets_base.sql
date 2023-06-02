61c5e883da19143f5b267c27527c54ac282b93c5
kane <kanelouise@users.noreply.github.com>
2023-06-02 12:12:51 -0700

    feat: make facebook_marketing full_refresh (#167)

--- dbt-cta/facebook_marketing/models/1_cta_full_refresh/ad_sets_base.sql
+++ dbt-cta/facebook_marketing/models/1_cta_full_refresh/ad_sets_base.sql
@@ -1,3 +1,8 @@
+{% set partitions_to_replace = [
+    "timestamp_trunc(current_timestamp, day)",
+    "timestamp_trunc(timestamp_sub(current_timestamp, interval 1 day), day)"
+] %}
+
 {{ config(
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
@@ -25,3 +30,7 @@ select
     ,budget_remaining
     ,effective_status
 from {{ ref('ad_sets_ab2') }}
+
+{% if is_incremental() %}
+where timestamp_trunc(_airbyte_emitted_at, day) in ({{ partitions_to_replace | join(",") }})
+{% endif %}
