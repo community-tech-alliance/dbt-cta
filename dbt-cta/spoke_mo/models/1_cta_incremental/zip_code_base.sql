281d89e809dbf63a8efcab749c6b2ab88ffb5eed
kane <kanelouise@users.noreply.github.com>
2023-06-23 15:13:33 -0700

    science: take out incremental suffix

--- dbt-cta/spoke_mo/models/1_cta_incremental/zip_code_base.sql
+++ dbt-cta/spoke_mo/models/1_cta_incremental/zip_code_base.sql
@@ -23,7 +23,5 @@ select
     _airbyte_zip_code_hashid
 from {{ ref('zip_code_ab3') }}
 -- zip_code from {{ source('cta', '_airbyte_raw_zip_code') }}
-{% if is_incremental() %}
-where timestamp_trunc(_airbyte_emitted_at, day) in ({{ partitions_to_replace | join(',') }})
-{% endif %}
+
 
