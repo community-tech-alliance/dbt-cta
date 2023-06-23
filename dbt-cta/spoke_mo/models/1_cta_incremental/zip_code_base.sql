a62925ae961927d53b661f73b3339a3f376daa00
kane <kanelouise@users.noreply.github.com>
2023-06-23 16:12:54 -0700

    science: add incremental back in + partitions_to_replace

--- dbt-cta/spoke_mo/models/1_cta_incremental/zip_code_base.sql
+++ dbt-cta/spoke_mo/models/1_cta_incremental/zip_code_base.sql
@@ -3,6 +3,7 @@
     'timestamp_trunc(timestamp_sub(current_timestamp, interval 1 day), day)'
 ] %}
 {{ config(
+    partitions = partitions_to_replace,
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
     unique_key = "zip"
@@ -23,5 +24,7 @@ select
     _airbyte_zip_code_hashid
 from {{ ref('zip_code_ab3') }}
 -- zip_code from {{ source('cta', '_airbyte_raw_zip_code') }}
-
+{% if is_incremental() %}
+where timestamp_trunc(_airbyte_emitted_at, day) in ({{ partitions_to_replace | join(',') }})
+{% endif %}
 
