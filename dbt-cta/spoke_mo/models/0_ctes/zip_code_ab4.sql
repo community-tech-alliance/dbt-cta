edee067c7b2e9c8866f7c40db2120d9b21ee07aa
kane <kanelouise@users.noreply.github.com>
2023-06-28 12:53:30 -0700

    feat: add unique row logic to zip code table

---
+++ dbt-cta/spoke_mo/models/0_ctes/zip_code_ab4.sql
@@ -0,0 +1,17 @@
+{{ config(
+    cluster_by = "_airbyte_emitted_at",
+    partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = 'zip_code'
+) }}
+
+-- SQL model to get latest `zip_code` values
+-- depends_on: {{ ref('zip_code_ab3') }}
+
+SELECT * FROM 
+(
+SELECT 
+    *,
+    ROW_NUMBER() OVER (PARTITION BY _airbyte_emitted_at ORDER BY zip_code desc) as rownum 
+FROM {{ ref('zip_code_ab3') }}
+)
+where rownum=1
