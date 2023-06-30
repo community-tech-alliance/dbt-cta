0960cb45f9e6e5ea53fcf253a54bb65217ae525a
kane <kanelouise@users.noreply.github.com>
2023-06-29 17:57:21 -0700

    typo: missing space

--- dbt-cta/spoke_mo/models/0_ctes/zip_code_ab4.sql
+++ dbt-cta/spoke_mo/models/0_ctes/zip_code_ab4.sql
@@ -1,7 +1,7 @@
 {{ config(
     cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = 'zip_code'
+    unique_key = 'zip'
 ) }}
 
 -- SQL model to get latest `zip_code` values
@@ -11,7 +11,7 @@ SELECT * FROM
 (
 SELECT 
     *,
-    ROW_NUMBER() OVER (PARTITION BY zip_code ORDER BY _airbyte_emitted_at desc) as rownum 
+    ROW_NUMBER() OVER (PARTITION BY zipORDER BY _airbyte_emitted_at desc) as rownum 
 FROM {{ ref('zip_code_ab3') }}
 )
 where rownum=1
