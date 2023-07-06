47418512b9f0e1511eb95d3ca40b163a1cca32ce
kane <kanelouise@users.noreply.github.com>
2023-07-06 12:51:58 -0700

    fix: add de-duping for zip code

--- dbt-cta/spoke_mo/models/0_ctes/zip_code_ab4.sql
+++ dbt-cta/spoke_mo/models/0_ctes/zip_code_ab4.sql
@@ -11,7 +11,7 @@ SELECT * FROM
 (
 SELECT 
     *,
-    ROW_NUMBER() OVER (PARTITION BY zipORDER BY _airbyte_emitted_at desc) as rownum 
+    ROW_NUMBER() OVER (PARTITION BY zip ORDER BY _airbyte_emitted_at desc) as rownum 
 FROM {{ ref('zip_code_ab3') }}
 )
 where rownum=1
