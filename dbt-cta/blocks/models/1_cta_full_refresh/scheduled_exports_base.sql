cca8e4ff80653d594d66943dc4e5645ee72c928f
royconst <roy@techallies.org>
2023-07-12 13:04:07 -0400

    converting scheduled exports base dbt file to reflect the change that will happen in airbyte

--- dbt-cta/blocks/models/1_cta_full_refresh/scheduled_exports_base.sql
+++ dbt-cta/blocks/models/1_cta_full_refresh/scheduled_exports_base.sql
@@ -1,7 +1,13 @@
+{% set partitions_to_replace = [
+    'timestamp_trunc(current_timestamp, day)',
+    'timestamp_trunc(timestamp_sub(current_timestamp, interval 1 day), day)'
+] %}
+
 {{ config(
-    cluster_by = ["_airbyte_emitted_at"],
+    partitions = partitions_to_replace,
+    cluster_by = "_airbyte_emitted_at",
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = "_airbyte_ab_id",
+    unique_key = '_airbyte_ab_id',
     tags = [ "top-level" ]
 ) }}
 
@@ -30,4 +36,6 @@ select
     _airbyte_scheduled_exports_hashid
 from {{ ref('scheduled_exports_ab3') }}
 -- scheduled_exports from {{ source('cta', '_airbyte_raw_scheduled_exports') }}
-
+{% if is_incremental() %}
+where timestamp_trunc(_airbyte_emitted_at, day) in ({{ partitions_to_replace | join(',') }})
+{% endif %}
