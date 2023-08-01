917cd21b658ae13b825635ee1105de7ab9c7a4e3
emily-flambe <emilycogsdill@gmail.com>
2023-08-01 13:19:20 -0400

    feat: add some models for workflow logs reporting

--- dbt-cta/monitoring/models/0_staging/workflows_execution_logs/stg_workflows_execution_logs.sql
+++ dbt-cta/monitoring/models/0_staging/workflows_execution_logs/stg_workflows_execution_logs.sql
@@ -2,8 +2,17 @@ with
     source as (select * from {{ source("workflows_logs", "executions") }}),
     unpack_nested_fields as (
         select
-            *,
+            * except (logName, resource, textPayload, jsonpayload_type_executionssystemlog),
             resource.type as resource_type,
+            resource.labels.resource_container as resource_container,
+            resource.labels.location as location,
+            resource.labels.workflow_id as workflow_id,
+            labels.workflows_googleapis_com_execution_id as execution_id,
+            labels.workflows_googleapis_com_revision_id as revision_id,
+            jsonpayload_type_executionssystemlog.state as state,
+            jsonpayload_type_executionssystemlog.start.argument as arguments,
+            jsonpayload_type_executionssystemlog.failure.source as failure_source,
+            jsonpayload_type_executionssystemlog.failure.exception as failure_exception,
             REGEXP_EXTRACT(logName, r"/([^/]+)/logs/") AS project_id
         from source
     ),
---
+++ dbt-cta/monitoring/models/1_intermediate/workflows_logs/int_workflow_logs.sql
@@ -0,0 +1,62 @@
+
+with
+    source as (select * from {{ ref("stg_workflows_execution_logs") }}),
+    workflow_metadata as (select * from {{ ref("stg_meta__workflow_mapping") }}),
+    /*
+    -- FOR TESTING
+    source as (
+        select
+            *
+        from `dev3869c056.monitoring.stg_workflows_execution_logs`
+        where workflow_id='sm-run-redshift-syncs'
+    ),
+    */
+    add_exec_order as (
+        select
+            *,
+            row_number() over (
+                partition by execution_id order by log_timestamp desc
+            ) as exec_order,
+            min(log_timestamp) over (
+                partition by execution_id
+            ) as execution_start_time,
+            max(log_timestamp) over (
+                partition by execution_id
+            ) as execution_finish_time,
+        from source
+    ),
+    latest_only as (
+        select
+            workflow_id,
+            execution_id,
+            log_timestamp,
+            state
+        from add_exec_order
+        where exec_order=1
+    ),
+    complete_source_data as (
+        select
+            a.log_timestamp,
+            a.project_id,
+            a.workflow_id,
+            a.execution_id,
+            a.exec_order,
+            a.state,
+            CASE WHEN a.state='FAILED' then 1 else 0 end as failure_flag, --include this in the final reporting view, which sums across error counts
+            a.arguments,
+            a.failure_source,
+            a.failure_exception,
+            a.execution_start_time,
+            CASE WHEN latest.state in ('FAILED','SUCCEEDED') then a.execution_finish_time ELSE null END as execution_finish_time,
+            datetime_diff(a.execution_finish_time, a.execution_start_time, minute) as runtime_minutes
+        from add_exec_order as a
+        left join latest_only as latest using(execution_id)
+    )
+
+select
+    source.*,
+    meta.sync,
+    meta.partner_name,
+    meta.data_type
+from complete_source_data as source
+left join workflow_metadata as meta using(workflow_id)
