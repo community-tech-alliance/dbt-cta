e367ca2405b630a45b0259dd4869faa8f966dd39
emily-flambe <emilycogsdill@gmail.com>
2023-08-23 17:52:51 -0400

    revert temp change that was for science oops

--- dbt-cta/bigquery_monitoring/models/1_intermediate/filtered_logs.sql
+++ dbt-cta/bigquery_monitoring/models/1_intermediate/filtered_logs.sql
@@ -1,4 +1,4 @@
 select 1 as colname
--- *
---from {{ source('cta','cloudaudit_googleapis_com_data_access') }}
---where resource.labels.project_id = '{{ env_var("PARTNER_PROJECT_ID") }}'
+ *
+from {{ source('cta','cloudaudit_googleapis_com_data_access') }}
+where resource.labels.project_id = '{{ env_var("PARTNER_PROJECT_ID") }}'
