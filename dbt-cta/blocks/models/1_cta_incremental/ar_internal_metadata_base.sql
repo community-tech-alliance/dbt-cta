c107caee942d3d28a0c334f91d0ffcfcb561e601
Michael Fisher <fisher@techallies.org>
2023-10-24 11:10:01 -0400

    hotfix: blocks hotkey ar_internal_metabase key -> hash (#250)

--- dbt-cta/blocks/models/1_cta_incremental/ar_internal_metadata_base.sql
+++ dbt-cta/blocks/models/1_cta_incremental/ar_internal_metadata_base.sql
@@ -1,7 +1,7 @@
 {{ config(
     cluster_by = ["_airbyte_emitted_at"],
     partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = 'id',
+    unique_key = '_airbyte_ar_internal_metadata_hashid',
     tags = [ "top-level" ]
 ) }}
 
