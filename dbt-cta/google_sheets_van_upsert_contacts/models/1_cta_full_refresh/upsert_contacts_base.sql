b29f0e7f53b6395a65953ef49e99b34a90f27c22
emily-flambe <emilycogsdill@gmail.com>
2023-11-26 16:45:04 -0500

    fix: make it a REAL full refresh model

--- dbt-cta/google_sheets_van_upsert_contacts/models/1_cta_full_refresh/upsert_contacts_base.sql
+++ dbt-cta/google_sheets_van_upsert_contacts/models/1_cta_full_refresh/upsert_contacts_base.sql
@@ -1,5 +1,21 @@
--- depends_on: {{ ref('ab1_upsert_contacts') }}
+{% set partitions_to_replace = [
+    "timestamp_trunc(current_timestamp, day)",
+    "timestamp_trunc(timestamp_sub(current_timestamp, interval 1 day), day)"
+] %}
+{{ config(
+    cluster_by = "_airbyte_emitted_at",
+    partition_by = {"field": "_airbyte_extracted_at", "data_type": "timestamp", "granularity": "day"},
+    partitions = partitions_to_replace,
+    unique_key = "_airbyte_raw_id"
+) }}
+
+-- Final base SQL model
+-- depends_on: {{ ref('upsert_contacts_ab1') }}
 
 select
     *
-from {{ ref('ab1_upsert_contacts') }}
+from {{ ref('upsert_contacts_ab1') }}
+
+{% if is_incremental() %}
+where timestamp_trunc(_airbyte_emitted_at, day) in ({{ partitions_to_replace | join(",") }})
+{% endif %}
