9f835e8689da5752bbd0a7d5999eacb10dc82e83
emily-flambe <emilycogsdill@gmail.com>
2023-11-30 09:38:01 -0500

    fix (quickbooks): missed three Quickbooks model reference replacements

--- dbt-cta/quickbooks/models/0_ctes/bill_payments_CreditCardPayment_CCAccountRef_ab1.sql
+++ dbt-cta/quickbooks/models/0_ctes/bill_payments_CreditCardPayment_CCAccountRef_ab1.sql
@@ -4,7 +4,7 @@
     unique_key = "_airbyte_ab_id"
 ) }}
 -- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
--- depends_on: {{ ref('bill_payments_CreditCardPayment') }}
+-- depends_on: {{ ref('bill_payments_CreditCardPayment_base') }}
 select
     _airbyte_CreditCardPayment_hashid,
     {{ json_extract_scalar('CCAccountRef', ['name'], ['name']) }} as name,
@@ -12,7 +12,7 @@ select
     _airbyte_ab_id,
     _airbyte_emitted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at
-from {{ ref('bill_payments_CreditCardPayment') }} as table_alias
+from {{ ref('bill_payments_CreditCardPayment_base') }} as table_alias
 -- CCAccountRef at bill_payments/CreditCardPayment/CCAccountRef
 where 1 = 1
 and CCAccountRef is not null
