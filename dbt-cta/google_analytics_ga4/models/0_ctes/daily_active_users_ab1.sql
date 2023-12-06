05535cfd47a3adaefbfc813682b5e9ec9f1b25b5
emily-flambe <emilycogsdill@gmail.com>
2023-12-06 11:24:21 -0500

    add models for GA4

---
+++ dbt-cta/google_analytics_ga4/models/0_ctes/daily_active_users_ab1.sql
@@ -0,0 +1,22 @@
+{{ config(
+    cluster_by = "_airbyte_extracted_at",
+    partition_by = {"field": "_airbyte_extracted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = '_airbyte_raw_id'
+) }}
+-- SQL model to build a hash column based on the values of this record
+-- depends_on: {{ source('cta','daily_active_users') }} 
+select
+    _airbyte_raw_id,
+    _airbyte_extracted_at,
+    _airbyte_meta,
+    date,
+    active1DayUsers,
+    property_id,
+    {{ dbt_utils.surrogate_key([
+    'date',
+    'active1DayUsers',
+    'property_id',
+    ]) }} as _airbyte_daily_active_users_hashid
+from {{ source('cta','daily_active_users') }}
+-- page
+where 1 = 1
