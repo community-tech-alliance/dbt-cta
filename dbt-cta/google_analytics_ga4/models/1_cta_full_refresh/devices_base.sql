14f47116f91130579a821b05e162eb8995f2df2c
emily-flambe <emilycogsdill@gmail.com>
2023-12-06 11:33:34 -0500

    add some models

---
+++ dbt-cta/google_analytics_ga4/models/1_cta_full_refresh/devices_base.sql
@@ -0,0 +1,35 @@
+{% set partitions_to_replace = [
+    "timestamp_trunc(current_timestamp, day)",
+    "timestamp_trunc(timestamp_sub(current_timestamp, interval 1 day), day)"
+] %}
+{{ config(
+    cluster_by = "_airbyte_extracted_at",
+    partition_by = {"field": "_airbyte_extracted_at", "data_type": "timestamp", "granularity": "day"},
+    partitions = partitions_to_replace,
+    unique_key = "_airbyte_devices_hashid"
+) }}
+
+-- Final base SQL model
+-- depends_on: {{ ref('devices_ab1') }}
+select
+    _airbyte_devices_hashid,
+    _airbyte_extracted_at,
+    _airbyte_meta,
+    date,
+    newUsers,
+    sessions,
+    sessionsPerUser,
+    totalUsers,
+    averageSessionDuration,
+    screenPageViewsPerSession,
+    operatingSystem,
+    bounceRate,
+    property_id,
+    deviceCategory,
+    browser,
+    screenPageViews,
+from {{ ref('devices_ab1') }}
+
+{% if is_incremental() %}
+where timestamp_trunc(_airbyte_extracted_at, day) in ({{ partitions_to_replace | join(",") }})
+{% endif %}
---
+++ dbt-cta/google_analytics_ga4/models/2_partner_matviews/four_weekly_active_users.sql
@@ -0,0 +1,2 @@
+select *
+from {{ source('cta','four_weekly_active_users_base') }}
