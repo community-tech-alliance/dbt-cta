75b8c57bb93cfc1e923cdc15c9e842c321772ba2
emily-flambe <emilycogsdill@gmail.com>
2023-12-08 09:43:56 -0500

    select base model(s) to generate and add sources.yml

--- utils/generate_dbt/generate_dbt.py
+++ utils/generate_dbt/generate_dbt.py
@@ -2,11 +2,15 @@ import os
 from google.cloud import bigquery
 from jinja2 import Environment, FileSystemLoader
 
-def create_directory_structure(base_dir, sync_name):
+def create_directory_structure(base_dir, sync_name, base_models_template_list):
     os.makedirs(os.path.join(base_dir, sync_name), exist_ok=True)
+
+    if "templates/1_cta_full_refresh.sql" in base_models_template_list:
+        os.makedirs(os.path.join(base_dir, sync_name, "models/1_cta_full_refresh"), exist_ok=True)
+    if "templates/1_cta_incremental.sql" in base_models_template_list:
+            os.makedirs(os.path.join(base_dir, sync_name, "models/1_cta_incremental"), exist_ok=True)
+
     os.makedirs(os.path.join(base_dir, sync_name, "models/0_ctes"), exist_ok=True)
-    os.makedirs(os.path.join(base_dir, sync_name, "models/1_cta_full_refresh"), exist_ok=True)
-    os.makedirs(os.path.join(base_dir, sync_name, "models/1_cta_incremental"), exist_ok=True)
     os.makedirs(os.path.join(base_dir, sync_name, "models/2_partner_matviews"), exist_ok=True)
     
     print(f"Created directory structure for sync: {sync_name}")
@@ -17,35 +21,35 @@ def extract_column_names(client, project_id, dataset_id, table_id):
     columns = [(row["column_name"], row["data_type"]) for row in query_job]
     return columns
 
-def generate_cte_sql_files(client, base_dir, sync_name, project_id, dataset_id, template_file):
+def generate_sql_files(client, base_dir, sync_name, project_id, dataset_id, template_file, template_name, file_suffix):
     dataset_ref = client.dataset(dataset_id, project=project_id)
     tables = client.list_tables(dataset_ref)
-    
-    env = Environment(loader=FileSystemLoader(os.path.dirname(template_file)))
-    template = env.get_template(os.path.basename(template_file))
-    
+
     for table in tables:
         table_id = table.table_id
-        
-        # Ignore tables ending in "_base"
-        if table_id.endswith("_base"):
+
+        # Skip tables that end with "_base" or start with "_"
+        if table_id.endswith("_base") or table_id.startswith("_"):
             continue
         
-        # Add "_ab1" suffix to the table file name
-        table_file_name = f"{table_id}_ab1.sql"
-        
+        # Generate the appropriate table file name based on the file_suffix
+        table_file_name = f"{table_id}{file_suffix}.sql"
+
+        # Determine the appropriate subdirectory based on the template file name
+        subdirectory = f"models/{template_name.replace('.sql', '')}"
+
         columns = extract_column_names(client, project_id, dataset_id, table_id)
-        
+
         # Create a list of all columns (no quotes, single indent after the first)
         all_columns = [field[0] for field in columns]
         all_columns_str = ",\n  ".join(all_columns)
-        
+
         # Create a list of columns with data types string, int, float, bool, date, or timestamp
         columns_for_hashid = [f"'{field[0]}'" for field in columns if field[1] in ['STRING', 'INT64', 'FLOAT64', 'BOOL', 'DATE', 'TIMESTAMP']]
-        
+
         # Indent twice for every element after the first without leading line break
         columns_for_hashid_str = ",\n  ".join(columns_for_hashid)
-        
+
         # Create a context dictionary with the variables you want to pass to the template
         context = {
             "all_columns": all_columns_str,
@@ -54,95 +58,77 @@ def generate_cte_sql_files(client, base_dir, sync_name, project_id, dataset_id, 
             "dataset": dataset_id,
             "table": table_id
         }
-        
+
         # Render the Jinja template with the context
+        env = Environment(loader=FileSystemLoader(os.path.dirname(template_file)))
+        template = env.get_template(os.path.basename(template_file))
         rendered_sql = template.render(**context)
-        
-        # Write the rendered SQL content to a file in the appropriate directory
-        file_path = os.path.join(base_dir, sync_name, f"models/0_ctes/{table_file_name}")
+
+        # Write the rendered SQL content to files in the specified subdirectory
+        file_path = os.path.join(base_dir, sync_name, subdirectory, table_file_name)
         with open(file_path, "w") as sql_file:
             sql_file.write(rendered_sql)
-        
-        print(f"Generated CTE model for table: {table_id}")
 
-def generate_base_sql_files(client, base_dir, sync_name, project_id, dataset_id, template_full_refresh, template_incremental):
+        print(f"Generated models for table: {table_id} in {subdirectory}")
+
+def generate_sources_yaml(client, base_dir, sync_name, project_id, dataset_id):
+    # Fetch the list of tables from the BigQuery dataset
     dataset_ref = client.dataset(dataset_id, project=project_id)
     tables = client.list_tables(dataset_ref)
+    table_list = [table.table_id for table in tables if not (table.table_id.endswith("_base") or table.table_id.startswith("_"))]
 
-    for table in tables:
-        table_id = table.table_id
+    # Define the path to the Jinja template for sources.yml
+    template_sources = "templates/sources.yml"
 
-        # Ignore tables ending in "_base"
-        if table_id.endswith("_base"):
-            continue
+    # Load the Jinja template
+    env = Environment(loader=FileSystemLoader(os.path.dirname(template_sources)))
+    template = env.get_template(os.path.basename(template_sources))
 
-        # Add "_base" suffix to the table file name
-        table_file_name = f"{table_id}_base.sql"
+    # Render the Jinja template with the table list
+    rendered_sources_yaml = template.render(table_list=table_list)
 
-        columns = extract_column_names(client, project_id, dataset_id, table_id)
+    # Write the rendered sources.yml to a file in the models folder
+    sources_yaml_path = os.path.join(base_dir, sync_name, "models", "sources.yml")
+    with open(sources_yaml_path, "w") as sources_yaml_file:
+        sources_yaml_file.write(rendered_sources_yaml)
 
-        # Create a list of all columns (no quotes, single indent after the first)
-        all_columns = [field[0] for field in columns]
-        all_columns_str = ",\n  ".join(all_columns)
-
-        # Create a list of columns with data types string, int, float, bool, date, or timestamp
-        columns_for_hashid = [f"'{field[0]}'" for field in columns if field[1] in ['STRING', 'INT64', 'FLOAT64', 'BOOL', 'DATE', 'TIMESTAMP']]
-
-        # Indent twice for every element after the first without leading line break
-        columns_for_hashid_str = ",\n  ".join(columns_for_hashid)
-
-        # Create a context dictionary with the variables you want to pass to the templates
-        context = {
-            "all_columns": all_columns_str,
-            "columns_for_hashid": columns_for_hashid_str,
-            "project": project_id,
-            "dataset": dataset_id,
-            "table": table_id
-        }
-
-        # Render the Jinja templates with the common context
-        env = Environment(loader=FileSystemLoader(os.path.dirname(template_full_refresh)))
-        template_full_refresh = env.get_template(os.path.basename(template_full_refresh))
-        rendered_sql_full_refresh = template_full_refresh.render(**context)
-
-        env = Environment(loader=FileSystemLoader(os.path.dirname(template_incremental)))
-        template_incremental = env.get_template(os.path.basename(template_incremental))
-        rendered_sql_incremental = template_incremental.render(**context)
-
-        # Determine the appropriate subdirectory based on the table name
-        if table_id.endswith("_incremental"):
-            subdirectory = "models/1_cta_incremental"
-        else:
-            subdirectory = "models/1_cta_full_refresh"
-
-        # Write the rendered SQL content to a file in the appropriate directory
-        file_path_full_refresh = os.path.join(base_dir, sync_name, subdirectory, table_file_name)
-        with open(file_path_full_refresh, "w") as sql_file_full_refresh:
-            sql_file_full_refresh.write(rendered_sql_full_refresh)
-
-        file_path_incremental = os.path.join(base_dir, sync_name, subdirectory, table_file_name)
-        with open(file_path_incremental, "w") as sql_file_incremental:
-            sql_file_incremental.write(rendered_sql_incremental)
-
-        print(f"Generated base models for table: {table_id}")
+    print("Generated sources.yml")
 
 def main():
     sync_name = input("Enter sync name (default: test)") or "test"
     project_id = input("Enter project ID: ")
     dataset_id = input("Enter dataset ID (default: dbt_gen_science): ") or "dbt_gen_science"
-    
+    base_models_option = input("Which base models do you need generated? (1=full_refresh, 2=incremental, 3 or skip to generate both): ") or '3'
+
     base_dir = "../../dbt-cta"
     template_ctes = "templates/0_ctes.sql"
     
     client = bigquery.Client()
-    
-    template_full_refresh = "templates/1_cta_full_refresh.sql"
-    template_incremental = "templates/1_cta_incremental.sql"
-    create_directory_structure(base_dir, sync_name)
-    generate_cte_sql_files(client, base_dir, sync_name, project_id, dataset_id, template_ctes)
-    generate_base_sql_files(client, base_dir, sync_name, project_id, dataset_id, template_full_refresh, template_incremental)
 
-    print("Script completed successfully.")
+    base_models_option_mapping = {
+        '1': ["templates/1_cta_full_refresh.sql"],
+        '2': ["templates/1_cta_incremental.sql"],
+        '3': ["templates/1_cta_full_refresh.sql","templates/1_cta_incremental.sql"]
+    }
+
+    base_models_template_list = base_models_option_mapping[base_models_option]
+
+    # create the folders needed
+    create_directory_structure(base_dir, sync_name, base_models_template_list)
+
+    # generate base models
+    for template_file in base_models_template_list:
+        template_name=os.path.splitext(os.path.basename(template_file))[0]
+        print(template_name)
+        #generate_sql_files(client, base_dir, sync_name, project_id, dataset_id, template_file, template_name, file_suffix = '_base')
+    
+    # generate CTE models
+    #generate_sql_files(client, base_dir, sync_name, project_id, dataset_id, template_file=template_ctes, template_name='0_ctes',file_suffix='_ab1')
+
+    # generate sources.yml
+    generate_sources_yaml(client, base_dir, sync_name, project_id, dataset_id)
+
+    print("Script completed")
 
 if __name__ == "__main__":
     main()
