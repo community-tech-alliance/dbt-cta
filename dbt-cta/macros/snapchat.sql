970cd40812b80fe6a7d03d10fbfda0c080573c32
emily-flambe <emilycogsdill@gmail.com>
2023-12-09 08:52:54 -0500

    update macros

--- dbt-cta/macros/snapchat.sql
+++
@@ -1,7 +0,0 @@
-{#
-    This macro extracts string arrays (if they exist) for a given geographic
-    targeting level. Expects a json column of geo information called "geos".
-#}
-{% macro extract_snapchat_geo(geo_value) -%}
-{{ json_extract_string_array(unnested_column_value("geos"), [geo_value], [geo_value]) }}
-{%- endmacro %}
