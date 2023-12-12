5a87a88db690f714d2e017ab789759fe4d41df2f
emily-flambe <emilycogsdill@gmail.com>
2023-12-12 10:27:10 -0500

    feat: update macros from Airbyte generated dbt (Dec 8 2023)

--- dbt-cta/macros/configuration.sql
+++ dbt-cta/macros/configuration.sql
@@ -3,10 +3,10 @@
         {{ return("") }}
     {%- endif -%}
 
-    {%- set table_schema, _, table_name = env_var("models_to_source")[this.identifier].partition(".") -%}
+    {%- set table_schema, _, table_name = var("models_to_source")[this.identifier].partition(".") -%}
 
     {%- call statement("get_column_type", fetch_result=True) -%}
-        select data_type from SVV_COLUMNS where table_name = '{{ table_name }}' and column_name = '{{ env_var("json_column") }}' and table_schema = '{{ table_schema }}';
+        select data_type from SVV_COLUMNS where table_name = '{{ table_name }}' and column_name = '{{ var("json_column") }}' and table_schema = '{{ table_schema }}';
     {%- endcall -%}
 
     {%- set column_type = load_result("get_column_type")["data"][0][0] -%}
