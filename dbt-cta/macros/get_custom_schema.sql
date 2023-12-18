2572e0e22603c763878848719ee8f23be059d5c1
Flambé <47834372+emily-flambe@users.noreply.github.com>
2023-12-18 10:35:35 -0500

    feat: update macros from Airbyte generated dbt (Dec 8 2023) (#270)
    * feat: update macros from Airbyte generated dbt (Dec 8 2023)
    
    * undo deletions
    
    * move get_custom_database to original location

--- dbt-cta/macros/get_custom_schema.sql
+++ dbt-cta/macros/get_custom_schema.sql
@@ -1,10 +1,4 @@
--- see
--- https://docs.getdbt.com/docs/building-a-dbt-project/building-models/using-custom-schemas/#an-alternative-pattern-for-generating-schema-names
+-- see https://docs.getdbt.com/docs/building-a-dbt-project/building-models/using-custom-schemas/#an-alternative-pattern-for-generating-schema-names
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-        {{ generate_schema_name_for_env(custom_schema_name, node) }}
-    {%- else -%}
-        {{ custom_schema_name }}
-    {%- endif -%}
+    {{ generate_schema_name_for_env(custom_schema_name, node) }}
 {%- endmacro %}
