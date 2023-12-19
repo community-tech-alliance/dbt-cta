80a4187b69f94eaaba15dc00a5c0ae332f68293d
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2023-12-19 10:31:42 -0600

    feat: remove redundant tests, add dedup cte to model that keeps failing

---
+++ dbt-cta/blocks_incremental/models/0_ctes/captricity_batches_ab4.sql
@@ -0,0 +1,17 @@
+{{ config(
+    cluster_by = "_airbyte_emitted_at",
+    partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = '_airbyte_ab_id',
+    tags = [ "top-level-intermediate" ]
+) }}
+-- SQL model to build a hash column based on the values of this record
+-- depends_on: {{ ref('captricity_batches_ab2') }}
+select * from
+    (
+        select
+            *,
+            row_number() over (partition by id order by updated_at desc) as rownum
+        from {{ ref('captricity_batches_ab2') }}
+    )
+where rownum = 1
+
