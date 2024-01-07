992e07d172f1289a297e96b233f199fe59d20629
emily-flambe <emilycogsdill@gmail.com>
2024-01-07 15:44:02 -0500

    add models and tests for `turf_vrrs` and `turf_pvqs`

---
+++ dbt-cta/blocks_incremental/models/0_ctes/turf_pvqs_ab1.sql
@@ -0,0 +1,21 @@
+{{ config(
+    cluster_by = "_airbyte_extracted_at",
+    partition_by = {"field": "_airbyte_extracted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = '_airbyte_raw_id'
+) }}
+-- SQL model to build a hash column based on the values of this record
+-- depends_on: {{ source('cta', 'turf_pvqs') }}
+
+select
+    _airbyte_raw_id,
+    _airbyte_extracted_at,
+    _airbyte_meta,
+    id,
+    phone_verification_question_id,
+    turf_id,
+   {{ dbt_utils.surrogate_key([
+        'id',
+        'phone_verification_question_id',
+        'turf_id'
+    ]) }} as _airbyte_turf_pvqs_hashid
+from {{ source('cta', 'turf_pvqs') }}
---
+++ dbt-cta/blocks_incremental/models/0_ctes/turf_vrrs_ab1.sql
@@ -0,0 +1,21 @@
+{{ config(
+    cluster_by = "_airbyte_extracted_at",
+    partition_by = {"field": "_airbyte_extracted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = '_airbyte_raw_id'
+) }}
+-- SQL model to build a hash column based on the values of this record
+-- depends_on: {{ source('cta', 'turf_vrrs') }}
+
+select
+    _airbyte_raw_id,
+    _airbyte_extracted_at,
+    _airbyte_meta,
+    id,
+    turf_id,
+    visual_review_response_id
+   {{ dbt_utils.surrogate_key([
+        'id',
+        'turf_id',
+        'visual_review_response_id'
+    ]) }} as _airbyte_turf_vrrs_hashid
+from {{ source('cta', 'turf_vrrs') }}
