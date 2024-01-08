93f15da4068405b079a321ea9c56e3b34b126cc8
Flambé <47834372+emily-flambe@users.noreply.github.com>
2024-01-08 17:41:16 -0500

    feat: automatically generate deduping CTEs when generating dbt from BQ V2 tables (#283)
    * yeet existing blocks_incremental files
    
    * add template and step to create _ab2 ctes
    
    * add param `destination_folder` to generate_sql_files()
    
    * re-add blocks_incremental models oops
    
    * remove all blocks_incremental files
    
    * re-add the yeeted models
    
    * add comment about nested fields in hashid

---
+++ utils/generate_dbt/templates/0_ctes_ab2.sql
@@ -0,0 +1,11 @@
+-- ensures the base model contains only one row per hashid
+-- this deduplicates data even if the source data contains duplicate rows
+
+SELECT * FROM 
+(
+SELECT 
+    *,
+    ROW_NUMBER() OVER (PARTITION BY _airbyte_{{ table }}_hashid ORDER BY _airbyte_extracted_at desc) as rownum 
+from {% raw %}{{ ref('{% endraw %}{{ table }}{% raw %}_ab1') }}{% endraw %}
+)
+where rownum=1
