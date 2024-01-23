f8614ad0b9a7a84e852bf5106046debedd4cf3ef
Flambé <47834372+emily-flambe@users.noreply.github.com>
2024-01-23 16:25:51 -0500

    style: run `sqlfluff` on sprout social sync (#292)
    style: run `sqlfluff`

--- dbt-cta/sprout_social/models/0_ctes/customer_profiles_ab2.sql
+++ dbt-cta/sprout_social/models/0_ctes/customer_profiles_ab2.sql
@@ -1,11 +1,11 @@
 -- ensures the base model contains only one row per hashid
 -- this deduplicates data even if the source data contains duplicate rows
 
-SELECT * FROM 
-(
-SELECT 
-    *,
-    ROW_NUMBER() OVER (PARTITION BY _airbyte_customer_profiles_hashid ORDER BY _airbyte_extracted_at desc) as rownum 
-from {{ ref('customer_profiles_ab1') }}
-)
-where rownum=1
+select * from
+    (
+        select
+            *,
+            row_number() over (partition by _airbyte_customer_profiles_hashid order by _airbyte_extracted_at desc) as rownum
+        from {{ ref('customer_profiles_ab1') }}
+    )
+where rownum = 1
--- dbt-cta/sprout_social/models/0_ctes/facebook_profile_analytics_ab3.sql
+++ dbt-cta/sprout_social/models/0_ctes/facebook_profile_analytics_ab3.sql
@@ -1,11 +1,11 @@
 -- ensures the base model contains only one row per hashid
 -- this deduplicates data even if the source data contains duplicate rows
 
-SELECT * EXCEPT (rownum) FROM 
-(
-SELECT 
-    *,
-    ROW_NUMBER() OVER (PARTITION BY _airbyte_facebook_profile_analytics_hashid ORDER BY _airbyte_extracted_at desc) as rownum 
-from {{ ref('facebook_profile_analytics_ab2') }}
-)
-where rownum=1
+select * except (rownum) from
+    (
+        select
+            *,
+            row_number() over (partition by _airbyte_facebook_profile_analytics_hashid order by _airbyte_extracted_at desc) as rownum
+        from {{ ref('facebook_profile_analytics_ab2') }}
+    )
+where rownum = 1
