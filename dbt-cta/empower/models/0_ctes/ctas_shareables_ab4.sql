ec75b7a1b21f51fffe7652bb708caff9dbca96d1
Frydafly <fryda.guedes@gmail.com>
2024-01-25 14:12:39 -0600

    fix: alter airbyte hash id partition by for empower models

--- dbt-cta/empower/models/0_ctes/ctas_shareables_ab4.sql
+++ dbt-cta/empower/models/0_ctes/ctas_shareables_ab4.sql
@@ -5,7 +5,7 @@ SELECT * EXCEPT (rownum) FROM
 (
 SELECT 
     *,
-    ROW_NUMBER() OVER (PARTITION BY _airbyte_ctas_shareables_hashid ORDER BY _airbyte_emitted_at desc) as rownum 
+    ROW_NUMBER() OVER (PARTITION BY _airbyte_shareables_hashid ORDER BY _airbyte_emitted_at desc) as rownum 
 from {{ ref('ctas_shareables_ab3') }}
 )
 where rownum=1
