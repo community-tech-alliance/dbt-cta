b454c6226422d7419ddb449499e5492a2f25e967
emily-flambe <emilycogsdill@gmail.com>
2024-01-30 17:43:52 -0500

    feat: sprout social models should be incremental, not full_refresh

---
+++ dbt-cta/sprout_social/models/1_cta_incremental/instagram_profile_analytics_base.sql
@@ -0,0 +1,10 @@
+{{ config(
+    cluster_by = "_airbyte_extracted_at",
+    partition_by = {"field": "_airbyte_extracted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = "_airbyte_instagram_profile_analytics_hashid"
+) }}
+
+-- Final base SQL model
+-- depends_on: {{ ref('instagram_profile_analytics_ab2') }}
+select * except (_airbyte_raw_id)
+from {{ ref('instagram_profile_analytics_ab2') }}
