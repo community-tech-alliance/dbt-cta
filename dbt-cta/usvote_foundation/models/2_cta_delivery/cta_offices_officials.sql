c8cccce74276e6f202df1e41d159af5a200edf28
emily-flambe <emilycogsdill@gmail.com>
2024-02-01 17:05:56 -0500

    remove model-level configs (usvote_foundation)

--- dbt-cta/usvote_foundation/models/2_cta_delivery/cta_offices_officials.sql
+++ dbt-cta/usvote_foundation/models/2_cta_delivery/cta_offices_officials.sql
@@ -1,6 +1,1 @@
-{{ config(
-	auto_refresh = false,
-	full_refresh = false
-) }}
-
 select * from {{ source('cta','offices_officials_base') }}
--- dbt-cta/usvote_foundation/models/2_partner_matviews/states.sql
+++ dbt-cta/usvote_foundation/models/2_partner_matviews/states.sql
@@ -1,6 +1,1 @@
-{{ config(
-	auto_refresh = false,
-	full_refresh = false
-) }}
-
 select * from {{ source('cta','states_base') }}
