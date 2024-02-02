0b56fd725f4e6c7b72c634eadf47386033b57c06
Flambé <47834372+emily-flambe@users.noreply.github.com>
2024-02-02 09:40:55 -0500

    cleanup: run `black` and remove one-time script for creating deduping CTEs (#317)
    * style: run `black`
    
    * remove code for one-time addition of deduping CTEs on old projects

--- utils/generate_dbt/old_script/generate_dbt_from_raw_tables.py
+++ utils/generate_dbt/old_script/generate_dbt_from_raw_tables.py
@@ -35,9 +35,8 @@ import yaml
 import zipfile
 from google.cloud import bigquery
 
-from dbt_file_contents import get_base_sql,\
-    get_base_config,\
-    get_matview_sql
+from dbt_file_contents import get_base_sql, get_base_config, get_matview_sql
+
 
 def get_spec_dict_from_file(spec_json_path):
     # open the JSON file in read mode
@@ -52,26 +51,22 @@ def get_spec_dict_from_file(spec_json_path):
 
 def main():
     # Parse them args
-    parser = argparse.ArgumentParser(description='Generate dbt files.')
-    parser.add_argument('--project_id',
-                        '-p',
-                        help='Google project id.'
-                        )
-    parser.add_argument('--dataset_id',
-                        '-d',
-                        help='Dataset id.'
-                        )
-    parser.add_argument('--output_path',
-                        '-o',
-                        help='Path to the output ZIP (default: dbt_models.zip)',
-                        default='dbt_models.zip'
-                        )
-    parser.add_argument('--spec_json_path',
-                        '-s',
-                        help='Path to the JSON containing sync modes,'
-                             ' unique keys, and cursor fields for each table',
-                        default='sample_table_spec.json'
-                        )
+    parser = argparse.ArgumentParser(description="Generate dbt files.")
+    parser.add_argument("--project_id", "-p", help="Google project id.")
+    parser.add_argument("--dataset_id", "-d", help="Dataset id.")
+    parser.add_argument(
+        "--output_path",
+        "-o",
+        help="Path to the output ZIP (default: dbt_models.zip)",
+        default="dbt_models.zip",
+    )
+    parser.add_argument(
+        "--spec_json_path",
+        "-s",
+        help="Path to the JSON containing sync modes,"
+        " unique keys, and cursor fields for each table",
+        default="sample_table_spec.json",
+    )
 
     args = parser.parse_args()
 
@@ -87,13 +82,12 @@ def main():
     tables = list(client.list_tables(dataset_id))
 
     # Include only tables with "raw" in the name
-    tables = [table for table in tables if 'raw' in table.table_id]
+    tables = [table for table in tables if "raw" in table.table_id]
 
     # Include only tables that are present in the table spec
     tables = [table for table in tables if table.table_id in spec_json_dict.keys()]
 
     with tempfile.TemporaryDirectory() as temp_dir_TODO:
-
         #######################
         # MAKE SOME DIRECTORIES
         #######################
@@ -105,10 +99,7 @@ def main():
         matviews_subdir = "2_partner_matviews"
 
         # Create the `models` subdirectory
-        models_path = os.path.join(temp_dir,
-                                   sync_dir,
-                                   models_dir
-                                   )
+        models_path = os.path.join(temp_dir, sync_dir, models_dir)
         os.makedirs(models_path, exist_ok=True)
 
         ####################
@@ -116,8 +107,9 @@ def main():
         ####################
 
         # Create the sources.yml file
-        source_tables_list = [{"name": table.table_id} for table in tables]\
-                             + [{"name": table.table_id.replace('raw','base')} for table in tables]
+        source_tables_list = [{"name": table.table_id} for table in tables] + [
+            {"name": table.table_id.replace("raw", "base")} for table in tables
+        ]
 
         sources_dict = {
             "version": 2,
@@ -126,32 +118,34 @@ def main():
                     "name": "cta",
                     "database": '{{ env_var("CTA_PROJECT_ID") }}',
                     "schema": '{{ env_var("CTA_DATASET_ID") }}',
-                    "tables": source_tables_list
+                    "tables": source_tables_list,
                 }
-            ]
+            ],
         }
 
         # Write the sources.yml file
         sources_yml_file_name = os.path.join(models_path, "sources.yml")
         with open(sources_yml_file_name, "w") as sources_yml_file:
-            yaml.dump(sources_dict,
-                      sources_yml_file,
-                      sort_keys=False,
-                      default_flow_style=False,
-                      default_style=''
-                      )
+            yaml.dump(
+                sources_dict,
+                sources_yml_file,
+                sort_keys=False,
+                default_flow_style=False,
+                default_style="",
+            )
 
         # Iterate over the tables and generate a SQL file for each
         # 'tables' has all the table names ending in _raw
         for table in tables:
-
             table_id_raw = table.table_id
-            table_id_base = table_id_raw.replace('raw', 'base')
+            table_id_base = table_id_raw.replace("raw", "base")
 
             # Load configs from the spec dict
-            unique_key = spec_json_dict[table_id_raw].get('unique_key', '_cta_hashid')
-            partition_datetime_field = spec_json_dict[table_id_raw].get('partition_datetime_field', '_cta_sync_datetime_utc')
-            sync_mode = spec_json_dict[table_id_raw].get('sync_mode', 'full_refresh')
+            unique_key = spec_json_dict[table_id_raw].get("unique_key", "_cta_hashid")
+            partition_datetime_field = spec_json_dict[table_id_raw].get(
+                "partition_datetime_field", "_cta_sync_datetime_utc"
+            )
+            sync_mode = spec_json_dict[table_id_raw].get("sync_mode", "full_refresh")
 
             # Get the schema for the _raw table
             raw_table_ref = f"{dataset_id}.{table_id_raw}"
@@ -160,8 +154,8 @@ def main():
             # Replace FLOAT with FLOAT64 field type
             fields_revised = []
             for field in table_schema:
-                if field.field_type == 'FLOAT':
-                    new_field = bigquery.SchemaField(field.name, 'FLOAT64')
+                if field.field_type == "FLOAT":
+                    new_field = bigquery.SchemaField(field.name, "FLOAT64")
                 else:
                     new_field = field
                 fields_revised.append(new_field)
@@ -172,9 +166,7 @@ def main():
 
             # Create the subdirectory if it doesn't already exist
             models_subdir = f"1_cta_{sync_mode}"
-            models_subdir_path = os.path.join(models_path,
-                                              models_subdir
-                                              )
+            models_subdir_path = os.path.join(models_path, models_subdir)
             os.makedirs(models_subdir_path, exist_ok=True)
 
             # Define the SQL query
@@ -183,21 +175,22 @@ def main():
                 for field in fields_revised
             )
 
-            concat_fields = ",\n                                        " \
-                .join(f"`{field.name}`" for field in table_schema)
+            concat_fields = ",\n                                        ".join(
+                f"`{field.name}`" for field in table_schema
+            )
 
             dbt_config = get_base_config(
                 partition_datetime_field=partition_datetime_field,
                 unique_key=unique_key,
-                sync_mode=sync_mode
+                sync_mode=sync_mode,
             )
 
             sql = get_base_sql(
-                    table_schema_fields=table_schema_fields,
-                    concat_fields=concat_fields,
-                    table_id=table_id_raw,
-                    partition_datetime_field=partition_datetime_field,
-                    sync_mode=sync_mode
+                table_schema_fields=table_schema_fields,
+                concat_fields=concat_fields,
+                table_id=table_id_raw,
+                partition_datetime_field=partition_datetime_field,
+                sync_mode=sync_mode,
             )
 
             # Write the dbt config and SQL query to the _base model file
@@ -213,32 +206,29 @@ def main():
 
             # Create the subdirectory if it doesn't already exist
             matviews_subdir = "2_partner_matviews"
-            matviews_subdir_path = os.path.join(models_path,
-                                              matviews_subdir
-                                              )
+            matviews_subdir_path = os.path.join(models_path, matviews_subdir)
             os.makedirs(matviews_subdir_path, exist_ok=True)
 
-            matview_fields = ",\n    ".join(
-                f"{field.name}"
-                for field in fields_revised
-            )
+            matview_fields = ",\n    ".join(f"{field.name}" for field in fields_revised)
 
             matview_sql = get_matview_sql(
                 matview_fields=matview_fields,
                 unique_key=unique_key,
-                table_id_base=table_id_base
-                )
+                table_id_base=table_id_base,
+            )
 
             # Name the matview model file
             # `table_id` = name of the table without _raw or _base
-            matview_file_path = os.path.join(matviews_subdir_path, f"{table_id_raw}.sql")
+            matview_file_path = os.path.join(
+                matviews_subdir_path, f"{table_id_raw}.sql"
+            )
 
             # Write the matview SQL to the file
             with open(matview_file_path, "w") as matview_file:
                 matview_file.write(matview_sql)
 
     # Write the contents of temp_dir to a ZIP file
-    with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile(output_path, "w", zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(temp_dir):
             for file in files:
                 # Get the full path of the file
@@ -249,5 +239,5 @@ def main():
     print(f"ZIP file '{output_path}' created.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
