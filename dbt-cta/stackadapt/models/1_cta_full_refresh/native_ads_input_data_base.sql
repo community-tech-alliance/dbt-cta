3c2b54fccfebc0cff6b58a96bec7937c9783926b
Frydafly <fryda.guedes@gmail.com>
2024-02-05 11:19:17 -0600

    [DE-640] feature: add deduping ctes to stackadapt (#312)
    feature: add deduping ctes to stackadapt
    
    FLAG: fails locally due to missing views of some sort
    
    Co-authored-by: Flambé <47834372+emily-flambe@users.noreply.github.com>

--- dbt-cta/stackadapt/models/1_cta_full_refresh/native_ads_input_data_base.sql
+++ dbt-cta/stackadapt/models/1_cta_full_refresh/native_ads_input_data_base.sql
@@ -8,7 +8,7 @@
     partitions=partitions_to_replace
 ) }}
 -- Final base SQL model
--- depends_on: {{ ref('native_ads_input_data_ab3') }}
+-- depends_on: {{ ref('native_ads_input_data_ab4') }}
 select
     _airbyte_native_ads_hashid,
     heading,
@@ -22,7 +22,7 @@ select
     _airbyte_emitted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at,
     _airbyte_input_data_hashid
-from {{ ref('native_ads_input_data_ab3') }}
+from {{ ref('native_ads_input_data_ab4') }}
 -- input_data at native_ads/input_data from {{ ref('native_ads_base') }}
 {% if is_incremental() %}
 where timestamp_trunc(_airbyte_emitted_at, day) in ({{ partitions_to_replace | join(',') }})
