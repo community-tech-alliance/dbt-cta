31985c420b5233b9538031e0523542ebc4e9e2ae
Frydafly <fryda.guedes@gmail.com>
2024-02-06 12:25:45 -0600

    feat: create sosha models and apply sqlfluff
    :d
    :w
    :sq
    :wq

---
+++ dbt-cta/sosha/models/0_ctes/campaign_insights_ab1.sql
@@ -0,0 +1,29 @@
+{{ config(
+    cluster_by = "_airbyte_extracted_at",
+    partition_by = {"field": "_airbyte_extracted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = '_airbyte_raw_id'
+) }}
+-- SQL model to build a hash column based on the values of this record
+-- depends_on: {{ source('cta', 'campaign_insights') }}
+
+select
+    _airbyte_raw_id,
+    _airbyte_extracted_at,
+    _airbyte_meta,
+    numberOfClicks,
+    numberOfShares,
+    topSharedTexts,
+    topSharedImages,
+    topClickedImages,
+    numberOfClicksByPlatform,
+    numberOfSharesByPlatform,
+    {{ dbt_utils.surrogate_key([
+    'numberOfClicks',
+    'numberOfShares',
+    'topSharedTexts',
+    'topSharedImages',
+    'topClickedImages',
+    'numberOfClicksByPlatform',
+    'numberOfSharesByPlatform'
+    ]) }} as _airbyte_campaign_insights_hashid
+from {{ source('cta', 'campaign_insights') }}
