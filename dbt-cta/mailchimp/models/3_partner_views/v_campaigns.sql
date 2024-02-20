d4cca4a10f4f4727d2ee158129d682fbc87cb0ef
emily-flambe <emilycogsdill@gmail.com>
2024-02-20 14:21:02 -0500

    cleanup: remove 3_partner_views

--- dbt-cta/mailchimp/models/3_partner_views/v_campaigns.sql
+++
@@ -1,5 +0,0 @@
---TODO(): reference this source from a variable configured in the config YAML
-
--- depends_on: {{ source('partner', 'campaigns') }}
-
-SELECT * FROM {{ source('partner','campaigns') }}
