94ddf246d0f1d7903b6f4aca6c2c91e5ae1b255a
Flambé <47834372+emily-flambe@users.noreply.github.com>
2024-02-23 16:08:07 -0500

    hotfix: add deduping CTEs to FB Pages sync (#335)
    feat: add deduping CTEs to FB Pages sync

--- dbt-cta/facebook_pages/models/1_cta_full_refresh/post_base.sql
+++ dbt-cta/facebook_pages/models/1_cta_full_refresh/post_base.sql
@@ -15,7 +15,7 @@
 ) }}
 
 -- Final base SQL model
--- depends_on: {{ ref('post_ab1') }}
+-- depends_on: {{ ref('post_ab2') }}
 select
     `_airbyte_post_hashid`,
     `_airbyte_extracted_at`,
@@ -32,7 +32,7 @@ select
     `message`,
     `permalink_url`,
     `actions`
-from {{ ref('post_ab1') }}
+from {{ ref('post_ab2') }}
 
 {% if is_incremental() %}
 where timestamp_trunc(_airbyte_extracted_at, day) in ({{ partitions_to_replace | join(",") }})
