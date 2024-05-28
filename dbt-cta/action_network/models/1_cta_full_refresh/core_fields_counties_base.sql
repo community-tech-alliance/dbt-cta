4f3a057a85b16b4cfabe24d507e30fb8c45cd865
Flambé <emilycogsdill@gmail.com>
2024-05-28 10:51:23 -0600

    set unique_key = "id" in full refresh models

--- dbt-cta/action_network/models/1_cta_full_refresh/core_fields_counties_base.sql
+++ dbt-cta/action_network/models/1_cta_full_refresh/core_fields_counties_base.sql
@@ -11,7 +11,7 @@
     cluster_by = "_airbyte_extracted_at",
     partition_by = {"field": "_airbyte_extracted_at", "data_type": "timestamp", "granularity": "day"},
     partitions = partitions_to_replace,
-    unique_key = "_airbyte_raw_id"
+    unique_key = "id"
 ) }}
 
 -- Final base SQL model
