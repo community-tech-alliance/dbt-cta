b694598b52ce71c9707c942e5de6205697d44918
Flambé <emilycogsdill@gmail.com>
2024-08-21 12:00:26 -0600

    use existing primary keys where available

--- dbt-cta/sprout_social/models/1_cta_incremental/client_metadata_base.sql
+++ dbt-cta/sprout_social/models/1_cta_incremental/client_metadata_base.sql
@@ -1,7 +1,7 @@
 {{ config(
     cluster_by = "_airbyte_extracted_at",
     partition_by = {"field": "_airbyte_extracted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = "_airbyte_client_metadata_hashid"
+    unique_key = "customer_id"
 ) }}
 
 -- Final base SQL model
