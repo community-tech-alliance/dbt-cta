016340c77ab73c837b938e77f3e0a2f85d1af90d
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2024-08-23 12:31:20 -0400

    feat: run dedupe cte on pk for echo19 (#401)
    feat: dedupe on pk for echo19

--- dbt-cta/echo19/models/0_ctes/text_contact_ab2.sql
+++ dbt-cta/echo19/models/0_ctes/text_contact_ab2.sql
@@ -5,7 +5,7 @@ select * except (rownum) from
     (
         select
             *,
-            row_number() over (partition by text_contact_hashid order by dateCreated desc) as rownum
+            row_number() over (partition by textResultId order by dateCreated desc) as rownum
         from {{ ref('text_contact_ab1') }}
     )
 where rownum = 1
