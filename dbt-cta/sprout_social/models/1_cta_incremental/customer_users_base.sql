fbc7f838693c482867c02fd0fe398d3f801d72f0
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2024-08-24 09:13:17 -0400

    Revert "sprout social should use existing primary keys for unique_key (+tests)" (#403)
    Revert "sprout social should use existing primary keys for unique_key (+tests…"
    
    This reverts commit 033d7cd54e9fdc3e41bd0c752eb26c3eccd8acb8.

--- dbt-cta/sprout_social/models/1_cta_incremental/customer_users_base.sql
+++ dbt-cta/sprout_social/models/1_cta_incremental/customer_users_base.sql
@@ -1,7 +1,7 @@
 {{ config(
     cluster_by = "_airbyte_extracted_at",
     partition_by = {"field": "_airbyte_extracted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = "id"
+    unique_key = "_airbyte_customer_users_hashid"
 ) }}
 
 -- Final base SQL model
