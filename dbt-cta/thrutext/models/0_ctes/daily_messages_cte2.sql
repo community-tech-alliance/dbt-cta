0fb46109be01d2bc32bc77561d4bf815e3fc33f3
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2024-09-26 17:17:45 -0500

    fix: use our loaded_at column instead of timestamp which is a custom field

--- dbt-cta/thrutext/models/0_ctes/daily_messages_cte2.sql
+++ dbt-cta/thrutext/models/0_ctes/daily_messages_cte2.sql
@@ -5,7 +5,7 @@ select * except (rownum) from
     (
         select
             *,
-            row_number() over (partition by _daily_messages_hashid order by timestamp desc) as rownum
+            row_number() over (partition by _daily_messages_hashid order by loaded_at desc) as rownum
         from {{ ref('daily_messages_cte1') }}
     )
 where rownum = 1
