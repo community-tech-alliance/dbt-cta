bdd129d3f4857796622e9e4c67e9babca0818196
Flambé <emilycogsdill@gmail.com>
2024-10-01 07:47:51 -0600

    Update inbox_addresses_cte1.sql

--- dbt-cta/hustle/models/0_ctes/inbox_addresses_cte1.sql
+++ dbt-cta/hustle/models/0_ctes/inbox_addresses_cte1.sql
@@ -3,17 +3,20 @@
 -- adds columns `_cta_hashid` and `_cta_sync_datetime_utc`
 
 select
-    cast(`created_at` as timestamp) as `created_at`,
-    cast(`type` as string) as `type`,
-    cast(`updated_at` as timestamp) as `updated_at`,
-    cast(`name` as string) as `name`,
-    cast(`id` as string) as `id`,
+
+    cast(`created_at` as timestamp) as created_at,
+    cast(`phone_number` as string) as phone_number,
+    cast(`inbound_routing_number` as string) as inbound_routing_number,
+    cast(`organization_id` as string) as organization_id,
+    cast(`updated_at` as timestamp) as updated_at,
+    cast(`id` as string) as id,
     to_hex(md5(concat(
         `created_at`,
-        `type`,
+        `phone_number`,
+        `inbound_routing_number`,
+        `organization_id`,
         `updated_at`,
-        `name`,
         `id`
     ))) as _cta_hashid,
     current_timestamp() as _cta_sync_datetime_utc
-from {{ source('cta', '_integrations_raw') }}
+from {{ source('cta', '_inbox_addresses_raw') }}
