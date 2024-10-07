1ab197342243f38d345f5f255bbe18ab6195a978
Huy Ngo <hngo4619@outlook.com>
2024-10-07 16:39:37 -0500

    feat: adding contact attempt for ddx

---
+++ dbt-cta/ddx/models/1_cta_incremental/contact_attempt_base.sql
@@ -0,0 +1,19 @@
+{{ config(
+    partition_by = {"field": "datetime_pulled", "data_type": "timestamp", "granularity": "day"},
+    unique_key = "_cta_hash_id",
+    tags = [ "contact-attempt" ],
+    persist_docs = {"columns": true, "relation": true}
+) }}
+
+select
+    person_id,
+    state_code,
+    contact_type_name,
+    contact_result_name,
+    record_id,
+    datetime_pulled,
+    datetime_window_start,
+    datetime_window_end,
+    _cta_loaded_at,
+    _cta_hash_id
+from {{ ref('contact_attempt_cte2') }}
