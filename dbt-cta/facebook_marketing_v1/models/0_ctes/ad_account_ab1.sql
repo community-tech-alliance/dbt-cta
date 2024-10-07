ca052dd8c9d7d2bdfa2435988ee07b5ae3398014
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2024-10-07 10:38:26 -0500

    feat: containerize dbt

--- dbt-cta/facebook_marketing_v1/models/0_ctes/ad_account_ab1.sql
+++ dbt-cta/facebook_marketing_v1/models/0_ctes/ad_account_ab1.sql
@@ -1,4 +1,4 @@
-{% set raw_table = env_var("CTA_DATASET_ID") ~ "_raw__stream_ad_account" %}
+{% set raw_table = env_var("CTA_DATASET_ID", "not-set") ~ "_raw__stream_ad_account" %}
 
 {{ config(
     cluster_by = "_airbyte_extracted_at",
