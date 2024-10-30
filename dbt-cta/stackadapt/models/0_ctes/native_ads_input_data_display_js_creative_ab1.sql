a371278d3e862114de9d8284e16bf762ea47e755
Frydafly <fryda.guedes@gmail.com>
2024-10-30 14:40:38 -0500

    fix: altered how input data comes through in json, so that the table will populate!

--- dbt-cta/stackadapt/models/0_ctes/native_ads_input_data_display_js_creative_ab1.sql
+++ dbt-cta/stackadapt/models/0_ctes/native_ads_input_data_display_js_creative_ab1.sql
@@ -4,18 +4,24 @@
     schema = "_airbyte_vfp_stackadapt_raw_v2",
     tags = [ "nested-intermediate" ]
 ) }}
+
 -- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
 -- depends_on: {{ ref('native_ads_input_data_base') }}
+{{ unnest_cte(ref('native_ads_input_data_base'), 'input_data', 'display_js_creative') }}
 select
     _airbyte_input_data_hashid,
-    JSON_EXTRACT_SCALAR(display_js_creative, '$.width') as width,
-    JSON_EXTRACT_SCALAR(display_js_creative, '$.height') as height,
-    JSON_EXTRACT_SCALAR(display_js_creative, '$.img_url') as img_url,
-    JSON_EXTRACT_SCALAR(display_js_creative, '$.js_code') as js_code,
-    JSON_EXTRACT_SCALAR(display_js_creative, '$.is_expandable') as is_expandable,
-    JSON_EXTRACT_SCALAR(display_js_creative, '$.js_code_macro') as js_code_macro,
+    {{ json_extract_scalar(unnested_column_value('display_js_creative'), ['width'], ['width']) }} as width,
+    {{ json_extract_scalar(unnested_column_value('display_js_creative'), ['height'], ['height']) }} as height,
+    {{ json_extract_scalar(unnested_column_value('display_js_creative'), ['img_url'], ['img_url']) }} as img_url,
+    {{ json_extract_scalar(unnested_column_value('display_js_creative'), ['js_code'], ['js_code']) }} as js_code,
+    {{ json_extract_scalar(unnested_column_value('display_js_creative'), ['is_expandable'], ['is_expandable']) }} as is_expandable,
+    {{ json_extract_scalar(unnested_column_value('display_js_creative'), ['js_code_macro'], ['js_code_macro']) }} as js_code_macro,
     _airbyte_raw_id,
     _airbyte_extracted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at
 from {{ ref('native_ads_input_data_base') }}
-where display_js_creative is not null
+-- display_js_creative at native_ads/input_data/display_js_creative
+{{ cross_join_unnest('input_data', 'display_js_creative') }}
+where
+    1 = 1
+    and display_js_creative is not null
