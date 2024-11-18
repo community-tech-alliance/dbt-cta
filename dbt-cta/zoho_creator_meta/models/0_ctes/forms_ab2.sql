3d58c0f03139285cbfa8e1784d4b5418dde14273
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2024-11-18 17:24:54 -0500

    feat: Zoho Creator to BQ V2 (#383)
    * feat: Zoho Creator to BQ V2
    
    * run linter'

--- dbt-cta/zoho_creator_meta/models/0_ctes/forms_ab2.sql
+++ dbt-cta/zoho_creator_meta/models/0_ctes/forms_ab2.sql
@@ -1,7 +1,7 @@
 {{ config(
-    cluster_by = "_airbyte_emitted_at",
-    partition_by = {"field": "_airbyte_emitted_at", "data_type": "timestamp", "granularity": "day"},
-    unique_key = "_airbyte_ab_id"
+    cluster_by = "_airbyte_extracted_at",
+    partition_by = {"field": "_airbyte_extracted_at", "data_type": "timestamp", "granularity": "day"},
+    unique_key = "_airbyte_raw_id"
 ) }}
 -- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
 -- depends_on: {{ ref('forms_ab1') }}
@@ -11,8 +11,8 @@ select
     cast(link_name as {{ dbt_utils.type_string() }}) as link_name,
     cast(display_name as {{ dbt_utils.type_string() }}) as display_name,
     cast(workspace_name as {{ dbt_utils.type_string() }}) as workspace_name,
-    _airbyte_ab_id,
-    _airbyte_emitted_at,
+    _airbyte_raw_id,
+    _airbyte_extracted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at
 from {{ ref('forms_ab1') }}
 -- forms
