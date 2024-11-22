f04e80479237d7bef920d2c75c4716641b974f48
Flambé <47834372+emily-flambe@users.noreply.github.com>
2024-11-22 10:20:21 -0700

    BQ V2 / Airbyte Cloud migration: Google Directory (#448)
    * initialize files for google_directory_bq_v2
    
    * oh I don't know anymore
    
    * update dbt in google_directory for BQ V2
    
    * Update sources.yml
    
    * run sqlfluff
    
    * ignore sqlfluff rule LT02
    
    * ignore ST06
    
    * appease the linter
    
    * Update users_emails_ab1.sql
    
    * sqlfluff you are KILLING ME.
    
    * god DAMN it fluff
    
    * look idk why my local fluffing isn't showing me these things but CI is

--- dbt-cta/google_directory/models/0_ctes/users_emails_ab1.sql
+++ dbt-cta/google_directory/models/0_ctes/users_emails_ab1.sql
@@ -1,16 +1,16 @@
 -- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
--- depends_on: {{ source('cta', 'users') }}
+-- depends_on: {{ ref('users_ab3') }}
 
 select
     _airbyte_users_hashid,
     json_extract_scalar(emails, "$['type']") as type,
     json_extract_scalar(emails, "$['address']") as address,
-    json_extract_scalar(emails, "$['primary']") as primary,
+    json_extract_scalar(emails, "$['primary']") as primary, --noqa
     json_extract_scalar(emails, "$['customType']") as customType,
-    _airbyte_ab_id,
-    _airbyte_emitted_at,
+    _airbyte_raw_id,
+    _airbyte_extracted_at,
     CURRENT_TIMESTAMP() as _airbyte_normalized_at
-from {{ source('cta', 'users') }} as table_alias
+from {{ ref('users_ab3') }}
 cross join unnest(emails) as emails
 where 1 = 1
 and emails is not null
