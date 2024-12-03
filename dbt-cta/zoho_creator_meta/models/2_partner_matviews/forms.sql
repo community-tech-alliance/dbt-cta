f61e9df67cbfd6970cc63625273f2390db9f5709
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2024-12-03 14:34:13 -0500

    feat: enable zoho creator analytics hub delivery (#468)

--- dbt-cta/zoho_creator_meta/models/2_partner_matviews/forms.sql
+++
@@ -1,9 +0,0 @@
-select
-    application_link_name,
-    type,
-    link_name,
-    display_name,
-    workspace_name,
-    _airbyte_extracted_at,
-    _airbyte_forms_hashid
-from {{ source('cta','forms_base') }}
