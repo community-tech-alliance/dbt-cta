a8d9aa052f69aeefbf27abf8f98749a775a12f14
Jesus Toquinto Zubiate <jesus@techallies.org>
2026-07-07 19:38:21 -0500

    Make scans_qc_overview incremental

--- dbt-cta/blocks_incremental/models/1_cta_incremental/scans_qc_overview_base.sql
+++ dbt-cta/blocks_incremental/models/1_cta_incremental/scans_qc_overview_base.sql
@@ -3,6 +3,6 @@
 ) }}
 
 -- Final base SQL model
--- depends_on: {{ ref('scans_qc_overview_ab1') }}
+-- depends_on: {{ ref('scans_qc_overview_ab2') }}
 select * except (_airbyte_raw_id)
-from {{ ref('scans_qc_overview_ab1') }}
+from {{ ref('scans_qc_overview_ab2') }}
